44a12df9a61026396e5e3727380f1098d85b1b30
rafatbd <rafatinfo4@gmail.com>
2018-02-23 14:29:57 +0100

    inici projecte

---
+++ inc/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  			<a class="navbar-brand" href="index.php">
+	  					<span class="navbar-brand mb-0 h1">*Inicio*</span>
+	  			</a>
+	  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    			<span class="navbar-toggler-icon"></span>
+	  			</button>
+	  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    			<ul class="navbar-nav mr-auto">
+	      				<li class="nav-item dropdown">
+	        				<a class="nav-link active dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Desplegable 1</a>
+	        					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	        					  <a class="dropdown-item" href="..." target="_blank">Info 1</a>
+	        					   <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					   <a class="dropdown-item" href="..." target="_blank">Info 2</a>
+	        					    <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					  <a class="dropdown-item" href="..." target="_blank">Info 3</a>
+	        					   <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					</div>
+	      				</li>
+	      		<li class="nav-item active">
+	        		<a class="nav-link" href="..." target="_blank">Contenido 2 *NUEVA PESTAÑA*</a>
+	      		</li>
+	      		<li class="nav-item active">
+	        		<a class="nav-link" href="...">Contenido 3 *SE CARGA EN LA MISMA PAG*</a>
+	      		</li>
+	    			</ul>
+	  			</div>
+</nav>
