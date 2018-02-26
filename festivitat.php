c8fb924f2a03bdd6dd34ce03fc682b682f422d58
belendiaz0808 <belendiazsoler0808@gmail.com>
2018-02-26 09:42:42 +0100

    festivitat

---
+++ festivitat.php
@@ -0,0 +1,12 @@
+<?php include("inc/head.php");?>
+	<div id="content" class="container-fluid">
+		<div class="row">			
+			<div class="col-6">
+				<img src="">    
+			</div>
+			<div class="col-6" id="texto">
+			</div>
+		</div>
+	</div>
+	
+<?php include("inc/footer.php");?>
