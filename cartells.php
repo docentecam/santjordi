9847aaa03b5f2dfdeb689dd8405911a5f50f212e
rafatbd <rafatinfo4@gmail.com>
2018-02-26 15:15:18 +0100

    cartells.php

---
+++ cartells.php
@@ -0,0 +1,376 @@
+<?php include("inc/head.php");?>
+		Opció 1:<br>
+		<div class="row">
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 1 fl1</h1>
+				<h1>alumne 1</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 2 fl1</h1>
+				<h1>alumne 2</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 3 fl1</h1>
+				<h1>alumne 3</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 4 fl1</h1>
+				<h1>alumne 4</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 1 fl2</h1>
+				<h1>alumne 1f</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 2 fl2</h1>
+				<h1>alumne 2f</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 3 fl2</h1>
+				<h1>alumne 3f</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-3 col-md-4 col-sm-12">
+				<img src="perfilalumne.jpg" class="img-fluid">
+				<h1>div 4 fl2</h1>
+				<h1>alumne 4f</h1>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>
+		</div>
+		<!-- TODO:descripció de cada imatge de la primera fila-->
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal offset-lg-2-->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<!-- TODO:descripció de cada imatge de la segona fila -->
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<!-- TODO:imatge gran personal -->
+				<h1>Imatge gran personal</h1>
+				<img src="perfilalumne400.jpg" class="img-fluid">
+			</div>
+			<div class="col-sm-12 col-md-6">
+				<div class="col-12">
+					<!-- TODO:imatge petit a sobre del text -->
+					<h2>Imatge petit sobre el text</h2>
+					<img src="perfilalumne150.jpg" class="img-fluid">
+				</div>
+				<div class="col-12">
+					<!-- TODO:text a sota de l'imatge -->
+					<!-- <pre> -->
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<!-- </pre> -->
+				</div>
+			</div>
+		</div>
+		Opció 2:<br>
+		<div class="row">
+			<div class="col-lg-6 col-md-4 col-sm-12">
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="perfilalumne.jpg" class="img-fluid">	
+					</div>
+					<div class="col-lg-4">
+						<h1>div 1 fl1</h1>
+						<h1>alumne 1</h1>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+						
+				</div>
+				
+			</div>
+			<div class="col-lg-6 col-md-4 col-sm-12">
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="perfilalumne.jpg" class="img-fluid">	
+					</div>
+					<div class="col-lg-4">
+						<h1>div 1 fl1</h1>
+						<h1>alumne 1</h1>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-lg-6 col-md-4 col-sm-12">
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="perfilalumne.jpg" class="img-fluid">	
+					</div>
+				<div class="col-lg-4">
+						<h1>div 1 fl1</h1>
+						<h1>alumne 1</h1>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				</div>
+				
+			</div>
+			<div class="col-lg-6 col-md-4 col-sm-12">
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="perfilalumne.jpg" class="img-fluid">
+					</div>
+					
+					<div class="col-lg-4">
+						<h1>div 4 fl1</h1>
+						<h1>alumne 4</h1>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+				
+				</div>
+			</div>
+		</div>
+		
+
+<?php include("inc/footer.php");?>
