0f25a674367b652c4e8b7c90def520ad9a061220
yaibondi <yaibondi@gmail.com>
2018-02-26 09:25:47 +0100

    les animacions creaciò

---
+++ animacions.php
@@ -0,0 +1,24 @@
+<?php include("inc/head.php");?>
+		<div class="row"> 
+		    <div class="col-sm-4">
+		      <h3>Column 1</h3>
+		      <p>Lorem ipsum dolor..</p>
+		      <p>Ut enim ad..</p>
+		    </div>
+		    <div class="col-sm-4">
+		      <h3>Column 2</h3>
+		      <p>Lorem ipsum dolor..</p>
+		      <p>Ut enim ad..</p>
+		    </div>
+		    <div class="col-sm-4">
+		      <h3>Column 3</h3> 
+		      <p>Lorem ipsum dolor..</p>
+		      <p>Ut enim ad..</p>
+		    </div>
+		    <div class="col-sm-4">
+		      <h3>Column 3</h3> 
+		      <p>Lorem ipsum dolor..</p>
+		      <p>Ut enim ad..</p>
+		    </div>	
+		</div>
+<?php include("inc/footer.php");?>
