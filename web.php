c5ef4c4eaf94d61fa0a8a6e9217f05efbad47b8d
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-02-27 08:06:02 +0100

    hecho

---
+++ web.php
@@ -0,0 +1,49 @@
+<?php include ("inc/head.php");?>
+
+<div class="page-header text-center">
+  <h1 >PROCÉS DE LA CREACIÓ DE LA PÁGINA </h1>
+</div>
+<div class="card mb-3">
+  <img class="card-img-top" src="img/matenimiento_renault.jpg" alt="Card image cap">
+  <div class="card-body bg-primary text-white">
+    <h5 class="card-title">HOLA! Som el grup de desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santiago i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
+    
+  </div>
+</div>
+<div   class="row">
+	<div class="col-6">
+		<img class="img-fluid" src="img/soc.png">
+	</div>	
+	<div class="card text-white bg-danger mb-3 col-6" >
+ 	 
+  	<div class="card-body">
+    <h5 id="textMeeting" class="card-title">Al iniciar el projecte vam fer una reunió entre els components del grup per establir i pensar idees per al projecte, el més important del projecte és sobretot tindre cohesió de grup per arribar al nostre objectiu final sense ningun problema.</h5>
+
+    </div>
+	</div>
+
+</div>
+<div class="row">
+	<div class="card text-white bg-warning mb-3 col-6" >
+	  
+	  <div class="card-body">
+	    <h5 id="textMockingboot" class="card-title">Mitjançant l'aplicació moockingboot creem un wireframe amb l'estructura amb els seus diferents elements: Menú amb les seves opcions, logos amb peu de pàgina, i el seu contingut amb cada opció del menú.
+	     </h5>
+				  
+	  </div>
+	</div>
+	<div class="col-6">
+		<img class="img-fluid" src="img/Capturaoscar.PNG">
+	</div>
+</div>
+
+<div class="row">
+	<div>
+		<img class="img-fluid" src="img/capturahangouts.jpg">
+		Després vam haver de fer una vídeo conferència amb l'aplicació Hangout per tal d'aclarir conceptes, fer canvis entre els tres projectes.
+
+	</div>
+</div>	
+
+
+<?php include("inc/footer.php");?>
