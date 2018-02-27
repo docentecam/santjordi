c0b0a24ffc34fde0e76801a2dc720a53b1dc7ebc
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-02-27 10:44:28 +0100

    tamaño movil tablet etc hechos

--- web.php
+++ web.php
@@ -1,6 +1,6 @@
 <?php include ("inc/head.php");?>
 
-<div class="page-header text-center">
+<div id="titleText" class="page-header text-center">
   <h1 >PROCÉS DE LA CREACIÓ DE LA PÁGINA </h1>
 </div>
 <div class="card mb-3">
@@ -11,14 +11,14 @@
   </div>
 </div>
 <div   class="row">
-	<div class="col-6">
-		<img class="img-fluid" src="img/soc.png">
+	<div class="  col-sm-12 col-md-12 col-lg-6 ">
+		<img class="img-fluid" src="img/reunionpaint.jpg">
 	</div>	
-	<div class="card text-white bg-danger mb-3 col-6" >
+	<div class="card text-white bg-danger mb-3 col-sm-12 col-md-12 col-lg-6 " >
  	 
   	<div class="card-body">
 
-    <h5 id="textMeeting" class="card-title">A l'iniciar el projecte, els components del grup ens vam reunir per fer una pluja d'idees per al projecte i així poder encaminar el projecte. Per poder fer aquesta pluja d'idees i encaminar el projecte el més important és poder tindre cohesió entre els membres del grup per així arribar a l'objectiu final sense cap problema. Un cop establit les idees principals ens vam posar a fer el Moockingbot.</h5>
+    <h5 id="textMeeting" class="card-title">A l'iniciar el projecte, els components del grup ens vam reunir per fer una pluja d'idees i així poder encaminar el projecte. Per poder fer aquesta pluja d'idees i encaminar el projecte el més important és poder tindre cohesió entre els membres del grup per així arribar a l'objectiu final sense cap problema. Un cop establit les idees principals ens vam posar a fer l'estructura en el Moockingbot.</h5>
 
 
     </div>
@@ -26,36 +26,32 @@
 
 </div>
 <div class="row">
-	<div class="card text-white bg-warning mb-3 col-6" >
+	<div class="card text-white bg-warning mb-3 col-sm-12 col-md-12 col-lg-6 " >
 	  
 	  <div class="card-body">
-	    <h5 id="textMockingboot" class="card-title">Ja en el Moockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
+	    <h5 id="textMockingboot" class="card-title">Ja en el Moockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
 	     </h5>
 				  
 	  </div>
 	</div>
-	<div class="col-6">
-		<img class="img-fluid" src="img/Capturaoscar.PNG">
+	<div id="pictureMocking" class="col-sm-12 col-md-12 col-lg-6">
+		<img  class="img-fluid" src="img/Capturaoscar.PNG">
 	</div>
 </div>
-<div class="row "><div class="row"></div>
-	<div class="col-3">
-		<img class="img-fluid " src="img/capturahangouts.jpg">
+<div class="row">
+	<div class="col-sm-12 col-md-12 col-lg-3">
+		<img class="img-fluid" src="img/capturahangouts.jpg">
 
 	</div>
-<div id="textHangout" class="card border-danger mb-3 col-3" style="max-width: 31rem;">
+<div  class="card border-danger mb-3 col-sm-12 col-md-12 col-lg-3" style="max-width: 31rem;">
   
-  <div class="card-body text-danger">
-    <h5 class="card-title">Després vam fer una vídeo conferència amb l'aplicació Hangout per tal d'aclarir conceptes, fer canvis entre les tres especialitats.Un cop parlat vam arribar a la conclusió de aplicar els canvis establerts entre tots.</h5>
+  <div  class="card-body text-danger">
+    <h5 id="textHangout" class="card-title">Després vam fer una vídeo conferència amb l'aplicació Hangout per tal d'aclarir conceptes, fer canvis entre les tres especialitats.Un cop parlat vam arribar a la conclusió de aplicar els canvis establerts entre tots.</h5>
     <p class="card-text"></p>
   </div>
 </div>
-<div class="col-3">asdasdfsdf</div>
-	
+</div>
 
-<div class="col-3">asdasdfasdf</div>
-
-</div>	
 
 
 
