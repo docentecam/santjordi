a84700c23cd3fc6ef3a4cca8e8807d259019ebb2
belendiaz0808 <belendiazsoler0808@gmail.com>
2018-02-27 08:07:45 +0100

    creació página festivitat

--- festivitat.php
+++ festivitat.php
@@ -1,12 +1,12 @@
 <?php include("inc/head.php");?>
 	<div id="content" class="container-fluid">
 		<div class="row">			
-			<div class="col-6">
-				<img src="">    
+			<div class=" col-sm-6  col-md-6 col-lg-6 col-xl-6">
+				<img src="img/santjordifestivitat.jpg">    
 			</div>
-			<div class="col-6" id="texto">
+			<div class=" col-sm-6  col-md-6 col-lg-6 col-xl-6" id="texto"><br/>
+				La festivitat consisteix en el fet que durant tot el dia de Sant Jordi (23 d'abril) es munten parades on venen llibres i roses; en molts llocs organitzen activitats no només el dia 23 sinó durant el cap de setmana o els dies previs al dia 23. També és un dia en què es té per costum regalar roses o llibres, o ambdues coses. Molts llocs organitzen firmes de llibres on van els escriptors a firmar els seus llibres, ja que han definit aquest dia com el dia internacional del llibre.  			
 			</div>
 		</div>
-	</div>
-	
+	</div><br/>	
 <?php include("inc/footer.php");?>
