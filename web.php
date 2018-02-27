cf31f8a60e16cdc54364a0201ff8f0ca0e6d3c96
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-02-27 14:16:38 +0100

    lo subo para ver errores

--- web.php
+++ web.php
@@ -1,20 +1,20 @@
 <?php include ("inc/head.php");?>
-
-<div id="titleText" class="page-header text-center">
-  <h1 >PROCÉS DE LA CREACIÓ DE LA PÁGINA </h1>
+<div class="row col-12 hidden-xs-down "></div>
+<div  class="page-header text-center">
+  <h1 class="txtTitles">Procés de la creació de la pàgina </h1>
 </div>
 <div class="card mb-3">
-  <img class="card-img-top" src="img/matenimiento_renault.jpg" alt="Card image cap">
-  <div class="card-body bg-primary text-white">
-    <h5 class="card-title">HOLA! Som el grup de desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santiago i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
+  <img class="card-img-top" src="img/cropped-cropped-3d-abstract-wallpaper-colorful-abstract-wallpapers.jpg" alt="Card image cap">
+  <div class="card-body bg-info text-dark">
+    <h5 class="card-title">HOLA! Som el grup de la especialitat desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santiago i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
     
   </div>
 </div>
-<div   class="row">
+<div  class="row">
 	<div class="  col-sm-12 col-md-12 col-lg-6 ">
-		<img class="img-fluid" src="img/reunionpaint.jpg">
+		<img class="img-fluid rounded" src="img/reunionpaint.jpg">
 	</div>	
-	<div class="card text-white bg-danger mb-3 col-sm-12 col-md-12 col-lg-6 " >
+	<div class="card text-dark  mb-3 col-sm-12 col-md-12 col-lg-6 " >
  	 
   	<div class="card-body">
 
@@ -26,7 +26,7 @@
 
 </div>
 <div class="row">
-	<div class="card text-white bg-warning mb-3 col-sm-12 col-md-12 col-lg-6 " >
+	<div class="card text-dark mb-3 col-sm-12 col-md-12 col-lg-6 " >
 	  
 	  <div class="card-body">
 	    <h5 id="textMockingboot" class="card-title">Ja en el Moockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
@@ -40,16 +40,16 @@
 </div>
 <div class="row">
 	<div class="col-sm-12 col-md-12 col-lg-3">
-		<img class="img-fluid" src="img/capturahangouts.jpg">
+		<img class="img-fluid rounded" src="img/capturahangouts.jpg">
 
 	</div>
-<div  class="card border-danger mb-3 col-sm-12 col-md-12 col-lg-3" style="max-width: 31rem;">
-  
-  <div  class="card-body text-danger">
-    <h5 id="textHangout" class="card-title">Després vam fer una vídeo conferència amb l'aplicació Hangout per tal d'aclarir conceptes, fer canvis entre les tres especialitats.Un cop parlat vam arribar a la conclusió de aplicar els canvis establerts entre tots.</h5>
-    <p class="card-text"></p>
-  </div>
-</div>
+	<div  class="card border-danger mb-3 col-sm-12 col-md-12 col-lg-3" style="max-width: 31rem;">
+	  
+	  <div  class="card-body text-danger">
+	    <h5 id="textHangout" class="card-title">A continuació fem una vídeo conferència entre les tres especialitats: Desenvolupament de web apps i d'app, dissenyadors/as 2D i 3D i animació de productes audiovisuals 2D i 3D mitjançant l'aplicació Hangout per tal d'aclarir conceptes. Un cop parlat vam començar d'aplicar els canvis establerts entre tots.</h5>
+	    <p class="card-text"></p>
+	  </div>
+	</div>
 </div>
 
 
