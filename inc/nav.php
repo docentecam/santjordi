b5035ebf646265c51abc85c0c3369fb3fb695397
rtalyon <rta.lyon@gmail.com>
2018-02-28 08:11:13 -0800

    correcion nav desplegable

--- inc/nav.php
+++ inc/nav.php
@@ -8,7 +8,7 @@
 	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    		<span class="navbar-toggler-icon"></span>
 	  		</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				 	<ul class="navbar-nav">
 						<li class="nav-item dropdown ml-lg-5 mr-lg-5">
 						    <a class="nav-link dropdown-toggle text-white active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sant Jordi</a>
