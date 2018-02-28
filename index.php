27c499bda93fa8486ee1045e47a06e01720b680d
rtalyon <rta.lyon@gmail.com>
2018-02-28 10:43:27 -0800

    eliminacion flechas carousel

--- index.php
+++ index.php
@@ -24,12 +24,10 @@
 	    </div>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselSantJordi" role="button" data-slide="prev">
-	   <!--  <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
-	    <span class="sr-only">Previous</span>
+	  	<span class="sr-only">Previous</span>
 	  </a>
 	  <a class="carousel-control-next" href="#carouselSantJordi" role="button" data-slide="next">
-	   <!--  <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
-	    <span class="sr-only">Next</span>
+	 	<span class="sr-only">Next</span>
 	  </a>
 	</div>
 	<?php include("inc/footer.php");?>
