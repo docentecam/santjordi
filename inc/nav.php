8e95a957789d1f07bfe11e1e7383c37b7b6067d7
Santi <santiwebdigitals@gmail.com>
2018-02-28 11:48:48 +0100

    nav cambios
    centrado con imagende fondo

--- inc/nav.php
+++ inc/nav.php
@@ -1,24 +1,28 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<div class="container col-3">												
-	  			<a class="navbar-brand" href="index.php">
-	  				 	<!-- <img src=""> LOGO HERE!!!-->
-	  					<span class="navbar-brand mb-0 h1">Sant Jordi</span>
-	  			</a>
-	  			</div>
-	  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	    			<span class="navbar-toggler-icon"></span>
-	  			</button>
-	  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-	    			<ul class="navbar-nav mr-auto">
-	 	      			<li class="nav-item active">
-							<a class="nav-link" href="..." target="_blank">Els Cartells</a>
-	 	      			</li>
-	 	      			<li>
-	        				<a class="nav-link active" href="..." target="_blank">Les Animacions</a>
-	      				</li>
-	      				<li class="nav-item active">
-	        				<a class="nav-link" href="..." target="_blank">La Web</a>
-	      				</li>
-	    			</ul>
-	  			</div>
+<nav id="navbgimg" class="navbar navbar-expand-lg navbar-light">
+	<div class="container mr-lg-5">
+		<div class="ml-lg-5">	
+	  		<a class="navbar-brand ml-lg-5 mr-lg-5" href="index.php">
+	  	 		<img src="img/iconbook.png"> 
+	  			<span class="navbar-brand mb-0 h1">Sant Jordi</span>
+	  		</a>
+	  	</div>
+	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+	  		</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+				 	<ul class="navbar-nav">
+						<li class="nav-item ml-lg-5 mr-lg-5">
+							<a class="nav-link" href="cartells.php">Els Cartells</a>
+						</li>
+						<li class="nav-item ml-lg-5 mr-lg-5">
+				   			<a class="nav-link" href="animacions.php">Les Animacions</a>
+				  		</li>
+				  		<li class="nav-item ml-lg-5 mr-lg-5">
+				   			<a class="nav-link" href="web.php">La Web</a>
+				  		</li>
+				 	</ul>
+				</div>	
+	</div>
 </nav>
+
+<div id="container" class="container-fluid">
