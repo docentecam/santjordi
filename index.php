80139110d3e5bad8461aab253e925f61113cba98
rtalyon <rta.lyon@gmail.com>
2018-03-01 10:06:33 -0800

    cambio del alt del index

--- index.php
+++ index.php
@@ -8,26 +8,26 @@
 	  </ol>
 	  <div class="carousel-inner">
 	    <div id="carouselImg1" class="carousel-item active ">
-	      <img class="img-fluid" src="img/carousel_ini_lg.png" alt="First slide">
+	      <img class="img-fluid" src="img/carousel_ini_lg.png" alt="gif d'un caballer llegint un llibre sobre un drac">
 	    </div>
 	    <div id="carouselImg2" class="carousel-item">
-	      <a href="llegenda.php"><img class="img-fluid" src="img/carousel_lleg_lg.png" alt="Second slide"></a>
+	      <a href="llegenda.php"><img class="img-fluid" src="img/carousel_lleg_lg.png" alt="Accès a la llegenda"></a>
 	      <div class="carousel-caption d-none d-md-block">
 		    <h5>Llegenda de Sant Jordi</h5>
 		  </div> 
 	    </div>
 	    <div id="carouselImg3" class="carousel-item">
-	     <a href="festivitat.php"><img class="img-fluid" src="img/carousel_fes_lg.png" alt="Third slide"></a>
+	     <a href="festivitat.php"><img class="img-fluid" src="img/carousel_fes_lg.png" alt="Accès a la festivitat"></a>
 	      <div class="carousel-caption d-none d-md-block">
 		    <h5>Festivitat de Sant Jordi</h5>
 		  </div>  
 	    </div>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselSantJordi" role="button" data-slide="prev">
-	  	<span class="sr-only">Previous</span>
+	  	<span class="sr-only">Enrere</span>
 	  </a>
 	  <a class="carousel-control-next" href="#carouselSantJordi" role="button" data-slide="next">
-	 	<span class="sr-only">Next</span>
+	 	<span class="sr-only">Endavant</span>
 	  </a>
 	</div>
 	<?php include("inc/footer.php");?>
