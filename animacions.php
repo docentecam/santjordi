dbbf552a695c1849694ddeb8bee55266661a750c
yaibondi <yaibondi@gmail.com>
2018-03-01 10:32:21 +0100

    corregio animacio
    12345678 de retrat y animacio

--- animacions.php
+++ animacions.php
@@ -4,42 +4,42 @@
 	</div>
 		<div class="row"> 
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio1_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 1</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio2_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 2</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio3_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 3</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio4_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 4</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio5_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 5</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio6_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 6</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio7_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 7</div>
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
-		    	<img class=" col-12 img-fluid" src="img/animacio1_lg.png">
+		    	<img class=" col-12 img-fluid" src="img/animacions/animacio8_lg.jpg">
 			  	<div class="col-12 text-center">Titol</div>
 			      <div class="col-12 text-center">alumne 8</div>
 		    </div>
@@ -47,7 +47,7 @@
 	
 		
 		<div class="row">
-		  	<div class="col-sm-12 col-md-6 offset-md-3 videostudent1">
+		  	<div class="col-sm-12 col-md-6 offset-md-3">
 			  	<div class="embed-responsive embed-responsive-16by9">
 	  				<iframe class="embed-responsive-item" src="video/animacioAlumne1_lg.mp4" allowfullscreen></iframe>
 				</div>
@@ -56,16 +56,16 @@
 		<h5 class="col-6 offset-md-3">opció1</h5>
 
 		<div class="row">
-			    <div class="col-4 col-md-2 offset-3 offset-md-3">
-			    	<img  class="img-fluid" src="img/retrat1_lg.jpg">
+			    <div class="col-4 col-md-2 offset-md-3">
+			    	<img  class="img-fluid" src="img/animacions/retrat1_lg.jpg">
 			    </div>
-				  <div class="col-6 mt-5">
+				  <div class="col-6">
 					    <span> nom  de l`alumne</span>
 					</div>
 		</div>
 				 
 		<div class="row">
-			<div class=" col-sm-12 col-6 offset-sm-3">
+			<div class=" col-sm-12 col-md-6 col-lg-6 offset-md-3">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -77,7 +77,7 @@
 
   		
   		<div class="row">
-		  	<div class="col-sm-12 col-md-6  offset-md-3 videostudent1">
+		  	<div class="col-sm-12 col-md-6  offset-md-3">
 			  	<div class="embed-responsive embed-responsive-16by9">
 	  				<iframe class="embed-responsive-item" src="video/animacioAlumne1_lg.mp4" allowfullscreen></iframe>
 				</div>
@@ -86,15 +86,15 @@
 		<h5 class="col-6 offset-md-3">opció2</h5> 
 
 		<div class="row">
-		    <div class="col-4 col-md-2 offset-3 offset-md-3">
-		    	<img class="img-fluid" src="img/retrat1_lg.jpg">
+		    <div class="col-4 col-md-2  offset-md-3">
+		    	<img class="img-fluid" src="img/animacions/retrat1_lg.jpg">
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-6 offset-md-3">	
 			   	<span> nom  de l`alumne</span>
 			</div>
-			<div class=" col-sm-12 col-lg-6  offset-md-3">
+			<div class="col-sm-12 col-md-6 col-lg-6 offset-md-3">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
