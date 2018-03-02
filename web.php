6285894024623340f3ab718e1fd8f5bc19737e57
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-03-02 12:19:36 +0100

    media screen hecho

--- web.php
+++ web.php
@@ -1,86 +1,69 @@
 <?php include ("inc/head.php");?>
 
-<div class="row ">
-	<div class="col-12 d-none d-sm-block txtTitles text-center">Procés de la creació de la pàgina</div>
+<div class="row  ">
+	<div class="col-12 d-none d-sm-block txtTitles text-center ">Procés de la creació de la pàgina</div>
 	
 </div>
 <div class="row">
 	<div class="col-12 col-md-12 card mb-3">
 	  <img id="borderImage" class="card-img-top" src="img/cropped-cropped-3d-abstract-wallpaper-colorful-abstract-wallpapers.jpg" alt="Card image cap">
 	  <div  id="titleColor" class="card-body  text-dark text-center">
-	    <h5 id="textHeadboard" class="card-title">HOLA! Som el grup de la especialitat desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santi i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
+	    <h5 class="card-title txtWeb">HOLA! Som el grup de la especialitat desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santi i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
 	    
 	  </div>
 	</div>
 </div>
-<div class="row">
+<div class="row txtWeb">
 	<div  class="col-sm-12  col-lg-6 ">
-		 <img id="meetingImage" class="img-fluid" src="img/reunionpaint.jpg">
+		 <img id="meetingImage" class="img-fluid" src="img/definicióProjecteoriginal.jpg">
 	</div>	
-	<div class="mb-3 col-sm-12 col-md-12 col-lg-6 " > 
-	  	<div>
-	    	<h5 id="textMeeting" class="card-title">A l'iniciar el projecte, els components del grup ens vam reunir per fer una pluja d'idees i així poder encaminar el projecte. Per poder fer aquesta pluja d'idees i encaminar el projecte el més important és poder tindre cohesió entre els membres del grup per així arribar a l'objectiu final sense cap problema. Un cop establit les idees principals ens vam posar a fer l'estructura en el Moockingbot.</h5>
-	    </div>
+	<div class=" col-sm-12 col-md-12 col-lg-6 mb-3 mt-5  txtWeb" > 
+	    	A l'iniciar el projecte, els components del grup ens vam reunir per fer una pluja d'idees i així poder encaminar el projecte. Per poder fer aquesta pluja d'idees i encaminar el projecte el més important és poder tindre cohesió entre els membres del grup per així arribar a l'objectiu final sense cap problema. Un cop establit les idees principals ens vam posar a fer l'estructura en el Moockingbot.
+	
 	</div>
 
 </div>
-<div class="row">
-	<div class="text-dark mb-3 col-sm-12 col-md-12 col-lg-6 mt-5" >
-	  
-	  <div>
-	    <h5 id="textMockingboot" >Ja en el Moockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
-	     </h5>
+<div class="row txtWeb">
+	<div class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb" >
+	    Ja en el Moockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
+	    
 				  
-	  </div>
 	</div>
 	<div class="col-sm-12 col-md-12 col-lg-6">
 		<img id="pictureMocking" class="img-fluid" src="img/Capturaoscar.PNG">
 	</div>
 </div>
-<div class="row">
+<div class="row txtWeb">
 	<div  class="col-sm-12 col-md-12 col-lg-6">
 		<img id="pictureHangout" class="img-fluid " src="img/Capturahangout3.JPG">
 
 	</div>
 	
-	<div  class="text-dark mb-3 col-sm-12 col-md-12 col-lg-6 mt-5">
-	  
-	  <div >
-	    <h5 id="textHangout">A continuació fem una vídeo conferència entre les tres especialitats: Desenvolupament de web apps i d'app, dissenyadors/as 2D i 3D i animació de productes audiovisuals 2D i 3D mitjançant l'aplicació Hangout per tal d'aclarir conceptes. Un cop parlat vam començar d'aplicar els canvis establerts entre tots.
-	     </h5>
-				  
-	  </div>
+	<div  class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb">
+	    A continuació fem una vídeo conferència entre les tres especialitats: Desenvolupament de web apps i d'app, dissenyadors/as 2D i 3D i animació de productes audiovisuals 2D i 3D mitjançant l'aplicació Hangout per tal d'aclarir conceptes. Un cop parlat vam començar d'aplicar els canvis establerts entre tots.
 	</div>
 	
 </div>
-<div class="row">
-	<div class="text-dark mb-3 col-sm-12 col-md-12 col-lg-6 mt-5" >
-	  
-	  <div>
-	    <h5 id="textFirezila" ">Un cop acabat el projecte, mitjançant el filezila pujem els arxius finalitzats al hosting que aixo fa que qualsevol persona entri a la pàgina.	
-	     </h5>
-				  
-	  </div>
+<div class="row txtWeb">
+	<div class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb" >
+	    Un cop acabat el projecte, mitjançant el filezila pujem els arxius finalitzats al hosting que aixo fa que qualsevol persona entri a la pàgina.	
 	</div>
 	<div class="col-sm-12 col-md-12 col-lg-6">
 		<img id="pictureFilezila" class="img-fluid" src="img/sitioFilezilla.png">
 	</div>
 	
 </div>
-<div class="row">
+<div class="row txtWeb">
 	<div  class="col-sm-12 col-md-12 col-lg-6 mb-5">
 		<img id="pictureWeb" class="img-fluid " src="img/sitioNavegador.png">
 
 	</div>
-	 <div  class="col-sm-12 col-md-12 col-lg-6 mt-5 text-center">
-		 <div>
-		    <h5 id="textWeb"> Finalment aquí teniu la pàgina ja pujada al hosting.	
-		     </h5>
-		    <div>
-		     	<img id="pictureArrow" src="img/flechaparaultimafoto.png" >
-			</div>		  
-		  </div>
-		</div> 
+	 <div  class="col-sm-12 col-md-12 col-lg-6 mt-5 text-center txtWeb">
+		    Finalment aquí teniu la pàgina ja pujada al hosting.	
+	</div>		    
+	<div class="col-sm-12 col-md-12 col-lg-">
+		<img id="pictureArrow" src="img/flechaparaultimafoto.png" >	  
+	</div> 
 </div>
 		
 	
