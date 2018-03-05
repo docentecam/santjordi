7f3c348ea62efbbc14395fbbf240f21af8d69742
belendiaz0808 <belendiazsoler0808@gmail.com>
2018-03-05 12:29:47 +0100

    canvis en festivitat

--- festivitat.php
+++ festivitat.php
@@ -1,12 +1,10 @@
 <?php include("inc/head.php");?>
 		<div class="row">		
-			<div class="col-6"><br/> <div class= "txtTitles text-center d-none d-sm-block">La festivitat al carrer</div><img src="img/santJordiCarrer_lg.png" class="col-12 img-fluid"></div>
-			<div class="col-sm-6 col-md-6 col-lg-6 col-xl-6" id="txtfestivitat">  
-				<br/>
-				<br/>
-				<br/>
-				<br/>
-					<p>
+			<div class="col-6"><br/><img src="img/santJordiCarrer_lg.png" class="col-12 img-fluid" ></div>
+			<div class="col-sm-6 col-md-6 col-lg-6 col-xl-6" id="txtfestivitat">
+				<br/><div class= "txtTitles text-center d-none d-sm-block">La festivitat al carrer</div>
+				 	
+					<p class="mr-3 ml-3">
 						La festivitat consisteix en el fet que durant tot el dia de Sant Jordi (23 d'abril) es munten parades on venen llibres i roses; en molts llocs organitzen activitats no només el dia 23 sinó durant el cap de setmana o els dies previs al dia 23. 
 						També és un dia en què es té per costum regalar roses o llibres, o ambdues coses. Molts llocs organitzen firmes de llibres on van els escriptors a firmar els seus llibres, ja que han definit aquest dia com el dia internacional del llibre.
 						Que no faltin tampoc les sardanes, els castellers, les exposicions... 
@@ -14,17 +12,13 @@
 						Aquest dia vol recordar el dia de la mort del cavaller Jordi. 
 						El sant, que restava sota les ordres de l’emperador Dioclecià, es va negar a seguir l’ordre de perseguir els cristians, per la qual cosa el van matar. 
 						Molt aviat el començaren a venerar com a màrtir i de seguida aparegueren històries fantàstiques lligades a la seva figura.
-
 					</p>
 			</div>			
 		</div><br/>
 		<div class="row">		
 			<div class=" col-sm-6  col-md-6 col-lg-6 col-xl-6 " id="txtescoles">
-					<br/>
-					<br/>
-					<br/>
-					<br/>
-					<p>
+					<div class= "txtTitles text-center d-none d-sm-block ">La festivitat a les escoles</div>
+					<p class="ml-3">
 						A moltes escoles per no dir a totes, també se celebra la Diada de Sant Jordi. 
 						Solen fer la típica demostració de la llegenda de Sant Jordi amb el seu drac, el seu príncep, la seva princesa... 
 						Que no falti el concurs literari que es fa, els nens i nenes creen el seu propi poema o la seva història que presenten. 
@@ -35,8 +29,7 @@
 						<br/>Amb aquesta celebració també volen commemorar la mort de Miquel de Cervantes  i de William Shakespeare, que van morir aquest mateix dia de 1616.
 					</p>
 			</div>
-
-			<div class=" col-6  "><div class= "txtTitles text-center d-none d-sm-block">La festivitat a les escoles</div><img src="img/santJordiEscola_lg.png" class="col-12 img-fluid" > </div>
+			<div class=" col-6 "><br/><img src="img/santJordiEscola_lg.png" class="col-12 img-fluid" > </div>
 	</div>	
 <br/>
 <?php include("inc/footer.php");?>
