f5218d4b9538fc02c87dda2346ea7a7dab6a7d83
Santi <santiwebdigitals@gmail.com>
2018-03-06 13:10:51 +0100

    ?

--- web.php
+++ web.php
@@ -5,9 +5,9 @@
 	
 </div>
 <div class="row">
-	<div class="col-12 col-md-12 card mb-3 order-1 borderRound">
-		<img  class="card-img-top" src="img/fotodelgrupopaint2.png" alt="Card image cap">
-		<div  id="titleColor" class="card-body  text-dark text-center borderRound">
+	<div class="col-12 col-md-12 card mb-3 order-1 ">
+		<img  id="firstPicture" class="card-img-top" src="img/fotodelgrupopaint2.png" alt="Card image cap">
+		<div  id="titleColor" class="card-body  text-dark text-center ">
 			<h5 class="card-title txtWeb ">HOLA! Som el grup de la especialitat desenvolupament d'app i web apps format per: Rafat, Andrea, Raul, Belén, Oscar, Santi i Claudia. Nosaltres hem sigut els creadors d'aquesta pàgina web</h5>
 		</div>
 	</div>
@@ -15,12 +15,12 @@
 	<div  class="col-sm-12  col-lg-6 order-2 ">
 		 <img  class="img-fluid borderRound" src="img/definicióProjecteoriginal.jpg">
 	</div>	
-	<div class=" col-sm-12 col-md-12 col-lg-6 mb-3 mt-5  txtWeb order-3"> 
+	<div class="col-sm-12 col-md-12 col-lg-6 mb-3 mt-5 txtWeb order-3"> 
 	    	<p>A l'iniciar el projecte, els components del grup ens vam reunir per fer una pluja d'idees i així poder encaminar el projecte. Per poder fer aquesta pluja d'idees i encaminar el projecte el més important és poder tindre cohesió entre els membres del grup per així arribar a l'objectiu final sense cap problema. Un cop establit les idees principals ens vam posar a fer l'estructura en el Mockingbot.</p>
 	</div>
 
 	<div class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb order-5 order-md-4">
-	    Ja en el Mockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.
+	    <p>Ja en el Mockingboot, utilitzem aquesta aplicació per crear un wireframe, que consisteix a fer una estructura visual de la pàgina web que farem amb els diferents elements que és compondrà la pàgina: Un menú en la capçalera amb les seves opcions, un peu de pàgina amb els logos corporatius, i el contingut de cada una de les opcions del menú.</p>
 	    
 				  
 	</div>
@@ -35,12 +35,12 @@
 	</div>
 	
 	<div  class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb order-7">
-	    A continuació fem una vídeo conferència entre les tres especialitats: Desenvolupament de web apps i d'app, dissenyadors/as 2D i 3D i animació de productes audiovisuals 2D i 3D mitjançant l'aplicació Hangout per tal d'aclarir conceptes. Un cop parlat vam començar d'aplicar els canvis establerts entre tots.
+	    <p>A continuació fem una vídeo conferència entre les tres especialitats: Desenvolupament de web apps i d'app, dissenyadors/as 2D i 3D i animació de productes audiovisuals 2D i 3D mitjançant l'aplicació Hangout per tal d'aclarir conceptes. Un cop parlat vam començar d'aplicar els canvis establerts entre tots.</p>
 	</div>
 	
 
 	<div class=" col-sm-12 col-md-12 col-lg-6 mt-5 mb-3 txtWeb order-9 order-md-8">
-	    Un cop acabat el projecte, mitjançant el filezila pujem els arxius finalitzats al hosting que aixo fa que qualsevol persona entri a la pàgina.	
+	    <p>Un cop acabat el projecte, mitjançant el filezila pujem els arxius finalitzats al hosting que aixo fa que qualsevol persona entri a la pàgina.</p>	
 	</div>
 	<div class="col-sm-12 col-md-12 col-lg-6 order-8 order-md-9">
 		<img  class="img-fluid borderRound" src="img/sitioFilezilla.png">
@@ -54,10 +54,11 @@
 	</div>
 	 <div  class="col-sm-12 col-md-12 col-lg-6 mt-5 order-11">
 	 	<div class="col-12 text-center txtWeb">
-		    Finalment aquí teniu la pàgina ja pujada al hosting.	
+		    <p>Finalment aquí teniu la pàgina ja pujada al hosting.	</p>
 		</div>
 		<div class="col-12 order-12">
-			<img id="pictureArrow" class="img-fluid " src="img/flechaparaultimafoto.png" >	  
+			<img id="pictureArrow " class="img-fluid d-md-block d-none" src="img/flechaparaultimafoto.png" >	  
+			<img  class="img-fluid d-md-none" src="img/Arrow_Up.png">
 		</div> 
 	</div>		    
 	
