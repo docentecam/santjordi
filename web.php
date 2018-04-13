b28a97cddc1012a15b625d4550d656b3e7f71446
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-04-13 15:04:36 +0200

    opcion 3 plantilla web hecha

--- web.php
+++ web.php
@@ -1,44 +1,46 @@
 <?php include ("inc/head.php");?>
 
 <div class="row ">
-	<div class="col-12 d-none d-sm-block txtTitles text-center mb-4">Procés de creació de la pàgina</div>
-	
+	<div class="col-12 d-none d-sm-block txtTitles text-center mb-5">Procés de creació de la pàgina</div>
 </div>
 <div class="row">
-	<div class="row">
-		<div class="col-12 col-lg-3"></div>
-		<div class="card col-12 col-lg-6" style="width: 18rem;">
-  <img class="card-img-top" src="img/fotodelgrupopaint2.png" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-text text-center">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa</h5>
-  </div>
+	<div class="col-lg-8 offset-2 mb-5 ">
+		<img class="img-fluid borderRound" src="img/fotodelgrupopaint2.png">
+		<h5 class="text-center ml-3 mr-3">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</h5>
+	</div>
 </div>
-	<div class="row">
-		
-	
-	
-
-	<div  class="col-12  col-lg-3 mb-3">
-		 <img  class="img-fluid borderRound" src="img/definicióProjecteoriginal.jpg">
-		 <h5 class="text-center">Iniciar el projecte</h5>
+<div class="row">
+	<div class="col-8 offset-2 ">
+		<div class="row">
+			<div class="col-lg mb-4">
+				<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
+			</div>
+			<div class="col-lg">
+				<h5>1-INICIAR EL PROJECTE</h5>
+			</div>
+			<div class="col-lg offset-1">
+				<img src="img/Capturamockingboot2.JPG" class="img-fluid borderRound">
+			</div>
+			<div class="col-lg">
+				<h5>2-CREAR UNA ESTRUCTURA</h5>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg">
+				<img src="img/Capturahangoutconferencia2.JPG" class="img-fluid borderRound">
+			</div>
+			<div class="col-lg">
+				<h5>3-POSAR EN COMÚ</h5>
+			</div>
+			<div class="col-lg offset-1">
+				<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound">
+			</div>
+			<div class="col-lg">
+				<h5>4-FINALITZAR EL PROJECTE</h5>
+			</div>
+		</div>
 	</div>
-	<div  class="col-12  col-lg-3 ">
-		 <img  class="img-fluid borderRound" src="img/Capturamockingboot2.JPG">
-		 <h5 class="text-center">Crear una estructura</h5>
-	</div>
-	<div  class="col-12  col-lg-3 ">
-		 <img  class="img-fluid borderRound" src="img/Capturahangout3.jpg">
-		 <h5 class="text-center">Posar en comú</h5>
-	</div>
-	<div  class="col-12  col-lg-3 ">
-		 <img  class="img-fluid borderRound" src="img/Capturafilezila4.JPG">
-		 <h5 class="text-center">Finalitzar el projecte</h5>
-	</div>
-	</div>
-	
-	
-</div>	
-
+</div>
 
 
 
