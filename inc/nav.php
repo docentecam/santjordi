a209da1401bc19b976484e7402294715fbefd37c
Santi <santiwebdigitals@gmail.com>
2018-04-13 13:34:58 +0200

    reorganizando items
    mejor responsive

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav id="navbgimg" class="navbar navbar-expand-lg navbar-dark bg-primary mb-2">
 	<div class="container">
-		<div class="mx-lg-2">	
-	  		<a class="navbar-brand mx-lg-5" href="index.php">
+		<div class="mx-lg-5">	
+	  		<a class="navbar-brand mx-lg-4" href="index.php">
 	  	 		<img src="img/navbaricon.png"> 
 	   		</a>
 	  	</div>
@@ -10,20 +10,20 @@
 	  		</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				 	<ul class="navbar-nav">
-						<li class="nav-item dropdown mx-lg-5 santmt3">
+						<li class="nav-item dropdown mx-lg-4 santmt3">
 						    <a class="nav-link dropdown-toggle active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sant Jordi</a>
 						    <div class="dropdown-menu">
 						      <a class="dropdown-item" href="llegenda.php"><span class="idiomes"><span>Llegenda</span><!-- <span>Leyenda</span> --></span></a>
 						      <a class="dropdown-item" href="festivitat.php">Festivitat</a>
 						    </div>
 						</li>
-						<li class="nav-item mx-lg-5">
+						<li class="nav-item mx-lg-4">
 							<a class="nav-link text-white" href="cartells.php"><span class="idiomes"><span>Els Cartells</span><!-- <span>Los Carteles</span> --></span></a>
 						</li>
-						<li class="nav-item mx-lg-5">
+						<li class="nav-item mx-lg-4">
 				   			<a class="nav-link text-white" href="animacions.php"><span class="idiomes"><span>Les Animacions</span><!-- <span>Las Animaciones</span> --></span></a>
 				  		</li>
-				  		<li class="nav-item mx-lg-5">
+				  		<li class="nav-item mx-lg-4">
 				   			<a class="nav-link text-white" href="web.php">La Web</a>
 				  		</li>
 				  		<li class="nav-item mt-lg-1 ml-lg-5 mr-lg-2 d-none d-lg-block">
