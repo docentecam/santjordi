d57853e6853c1209ad15baee504ceb1de4f08ca6
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-04-13 13:12:40 +0200

    opcion 2 web hecha

--- web.php
+++ web.php
@@ -1,32 +1,43 @@
 <?php include ("inc/head.php");?>
 
-<div class="row">
-	<div class="col-12 d-none d-sm-block txtTitles text-center ">Procés de creació de la pàgina</div>
+<div class="row ">
+	<div class="col-12 d-none d-sm-block txtTitles text-center mb-4">Procés de creació de la pàgina</div>
 	
 </div>
 <div class="row">
-	<div  class="col-sm-12  col-lg-3 order-2 ">
+	<div class="row">
+		<div class="col-12 col-lg-3"></div>
+		<div class="card col-12 col-lg-6" style="width: 18rem;">
+  <img class="card-img-top" src="img/fotodelgrupopaint2.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-text text-center">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa</h5>
+  </div>
+</div>
+	<div class="row">
+		
+	
+	
+
+	<div  class="col-12  col-lg-3 mb-3">
 		 <img  class="img-fluid borderRound" src="img/definicióProjecteoriginal.jpg">
 		 <h5 class="text-center">Iniciar el projecte</h5>
 	</div>
-	<div  class="col-sm-12  col-lg-3 order-2 ">
+	<div  class="col-12  col-lg-3 ">
 		 <img  class="img-fluid borderRound" src="img/Capturamockingboot2.JPG">
 		 <h5 class="text-center">Crear una estructura</h5>
 	</div>
-	<div  class="col-sm-12  col-lg-3 order-2 ">
+	<div  class="col-12  col-lg-3 ">
 		 <img  class="img-fluid borderRound" src="img/Capturahangout3.jpg">
 		 <h5 class="text-center">Posar en comú</h5>
 	</div>
-	<div  class="col-sm-12  col-lg-3 order-2 ">
+	<div  class="col-12  col-lg-3 ">
 		 <img  class="img-fluid borderRound" src="img/Capturafilezila4.JPG">
 		 <h5 class="text-center">Finalitzar el projecte</h5>
 	</div>
-	<div >
-		<img>
 	</div>
-</div>
-		
 	
+	
+</div>	
 
 
 
