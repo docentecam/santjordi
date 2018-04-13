25de06dd7251f8bcbd5b47bc7cf19c4cffa04329
Santi <santiwebdigitals@gmail.com>
2018-04-13 13:15:28 +0200

    Bandera española y catalana
    flag cambiar idioma

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,6 @@
 <nav id="navbgimg" class="navbar navbar-expand-lg navbar-dark bg-primary mb-2">
 	<div class="container">
-		<div class="mx-lg-3">	
+		<div class="mx-lg-2">	
 	  		<a class="navbar-brand mx-lg-5" href="index.php">
 	  	 		<img src="img/navbaricon.png"> 
 	   		</a>
@@ -13,19 +13,25 @@
 						<li class="nav-item dropdown mx-lg-5 santmt3">
 						    <a class="nav-link dropdown-toggle active" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Sant Jordi</a>
 						    <div class="dropdown-menu">
-						      <a class="dropdown-item" href="llegenda.php">Llegenda</a>
+						      <a class="dropdown-item" href="llegenda.php"><span class="idiomes"><span>Llegenda</span><!-- <span>Leyenda</span> --></span></a>
 						      <a class="dropdown-item" href="festivitat.php">Festivitat</a>
 						    </div>
 						</li>
 						<li class="nav-item mx-lg-5">
-							<a class="nav-link text-white" href="cartells.php">Els Cartells</a>
+							<a class="nav-link text-white" href="cartells.php"><span class="idiomes"><span>Els Cartells</span><!-- <span>Los Carteles</span> --></span></a>
 						</li>
 						<li class="nav-item mx-lg-5">
-				   			<a class="nav-link text-white" href="animacions.php">Les Animacions</a>
+				   			<a class="nav-link text-white" href="animacions.php"><span class="idiomes"><span>Les Animacions</span><!-- <span>Las Animaciones</span> --></span></a>
 				  		</li>
 				  		<li class="nav-item mx-lg-5">
 				   			<a class="nav-link text-white" href="web.php">La Web</a>
 				  		</li>
+				  		<li class="nav-item mt-lg-1 ml-lg-5 mr-lg-2 d-none d-lg-block">
+				   			<a href="aun no?¿?"><img src="img/23px-Flag_of_Spain.png"></a>
+				  		</li>
+						<li class="nav-item mt-lg-1 d-none d-lg-block">
+				   			<a href="aun no?¿?¿"><img src="img/23px-Flag_of_Catalonia.png"></a>
+				  		</li>	
 				 	</ul>
 				</div>	
 	</div>
