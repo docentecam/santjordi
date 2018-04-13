14b0ce218696b2c097b3a422a809102343effa22
rtalyon <rta.lyon@gmail.com>
2018-04-13 14:09:49 +0200

    titulos dentro del carousel

--- index.php
+++ index.php
@@ -9,18 +9,35 @@
 	  <div class="carousel-inner">
 	    <div id="carouselImg1" class="carousel-item text-center active ">
 	      <a href="animacions.php"><img class="img-fluid imgSize fill d-none d-md-block" src="img/carousel_ini_lg.gif" alt="gif d'un caballer llegint un llibre sobre un drac"></a>
-	      <img class="img-fluid d-md-none" src="img/carousel_ini_lg.gif" alt="gif d'un caballer llegint un llibre sobre un drac">
+	      <div class="carousel-caption d-none d-md-block">
+		    <h5>
+		    	<span class="idiomes">
+		    		<span>Animacions</span>
+		    		<span>Animaciones</span>
+		    	</span>
+		    </h5>
+		  </div> 
 	    </div>
 	    <div id="carouselImg2" class="carousel-item">
 	      <a href="llegenda.php"><img class="img-fluid" src="img/carousel_lleg_lg.png" alt="Accès a la llegenda"></a>
 	      <div class="carousel-caption d-none d-md-block">
-		    <h5>Llegenda de Sant Jordi</h5>
+		    <h5>
+		    	<span class="idiomes">
+		    		<span>Llegenda de Sant Jordi</span>
+		    		<span>Leyenda de Sant Jordi</span>
+		    	</span>
+		    </h5>
 		  </div> 
 	    </div>
 	    <div id="carouselImg3" class="carousel-item">
 	     <a href="festivitat.php"><img class="img-fluid" src="img/carousel_fes_lg.png" alt="Accès a la festivitat"></a>
 	      <div class="carousel-caption d-none d-md-block">
-		    <h5>Festivitat de Sant Jordi</h5>
+		      <h5>
+		    	<span class="idiomes">
+		    		<span>Festivitad de Sant Jordi</span>
+		    		<span>Festividad de Sant Jordi</span>
+		    	</span>
+		    </h5>
 		  </div>  
 	    </div>
 	  </div>
