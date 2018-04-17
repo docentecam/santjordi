a059077d437d0e8ba06df49b807fa512575266e6
claudiadel <cdelbarr@gmail.com>
2018-04-17 15:08:00 +0200

    Cambio div a h1

--- llegenda.php
+++ llegenda.php
@@ -3,7 +3,7 @@
 		<div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2 mt-3 mb-3 px-5 py-2" id="llegenda">
 			<h1 class="txtTitles">La llegenda de Sant Jordi</h1>
 			<div class="row">
-				<div class="col-sm-6 col-md pr-5 order-2 order-md-0">
+				<div class="col-sm-12 col-md pr-5 order-2 order-md-0">
 					<p class="txtBorders"><span class="firstcharacter">L</span>eyendas de Sant Jordi hay muchas, y todas muy distintas, pero a nuestros oídos ha llegado una muy peculiar. Cuenta la leyenda que en las afueras de una pequeña aldea acechaba un terrible dragón que atemorizaba a todos los habitantes. Con sus grandes colmillos era capaz de devorar a todo el que se le pusiera por delante. Al pueblo se le ocurrió hacer un sorteo diario entre todos los habitantes y la persona que saliera sería la presa del dragón, para que así, a cambio de sacrificar a alguien, dejara a todos tranquilos. Y así se hizo durante mucho tiempo, hasta que un día la elegida en el sorteo fue la hija del rey. El rey, dolido, aprobó el sacrificio de su hija, y ésta se fue a las afueras para encontrarse con ese feroz dragón. De pronto, la princesa vio aparecer a un joven y apuesto caballero que estaba allí expresamente para matar al dragón y salvar a la princesa, y, tras una batalla entre el dragón y el caballero, finalmente el caballero hundió su espada en el cuerpo de la bestia. De la sangre que salió del animal, brotó un rosal, con las rosas más rojas jamás vistas. El caballero le regaló a la princesa una rosa y, tras llevarla al pueblo con el dragón a cargas como símbolo de victoria, el caballero desapareció misteriosamente y sin dejar rastro.</p class="txtBorders">
 					
 				</div>
