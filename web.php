2c7070a1099abb5baea564a490d0ce2f3f25b966
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-04-17 13:19:56 +0200

    ultima plantilla establecida

--- web.php
+++ web.php
@@ -1,6 +1,6 @@
 <?php include ("inc/head.php");?>
 
-<div class="row ">
+<div class="row">
 	<div class="col-12 d-none d-sm-block txtTitles text-center mb-5">Procés de creació de la pàgina</div>
 </div>
 <div class="row">
@@ -9,8 +9,7 @@
 		<h5 class="text-center ml-3 mr-3">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</h5>
 	</div>
 </div>
-<div class="row">
-	<div class="col-8 offset-2 ">
+	<div class="col-8 offset-2">
 		<div class="row">
 			<div class="col-lg-5 mb-4 mt-3">
 				<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
@@ -19,7 +18,7 @@
 			
 				
 			
-			<div class="col-lg-5 offset-1 mt-3">
+			<div class="col-lg-5 offset-2 mt-3">
 				<img src="img/Capturamockingboot2.JPG" class="img-fluid borderRound">
 					<h5 class="text-center">2-CREAR UNA ESTRUCTURA</h5>
 			</div>
@@ -33,7 +32,7 @@
 			
 				
 			
-			<div class="col-lg-5 offset-1 mt-3">
+			<div class="col-lg-5 offset-2 mt-3">
 				<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound">
 					<h5 class="text-center">4-FINALITZAR EL PROJECTE</h5>
 			</div>
@@ -42,7 +41,30 @@
 			
 		</div>
 	</div>
+
+
+
+<div class="col-12 col-lg-6">
+	<p>
+		Per començar el projecte, els components del grup ens vam reunir per fer una pluja d’idees o brainstorming. Dins d’aquesta primera part es plantegen les característiques de la pàgina web que volem crear, des de la seva finalitat (explicar la llegenda de Sant Jordi, la festivitat, etc) fins a la quantitat d’informació que volem afegir (imatges, textos, etc).
+	</p>
 </div>
+<div class="col-12 col-lg-6">
+	<p>
+		A l’hora de fer un esquema de com volem que sigui la pàgina, fem servir el programa Mockingbot. Aquest programa ens dóna eines que ens faciliten crear l’estructura o wireframe, on apareixeran tots els elements visuals de la web. Els elements poden anar des d’un menú a la capçalera amb opcions de navegació, a un peu de pàgina amb els logotips corporatius, entre altres.
+	</p>
+</div>
+<div class="col-12 col-lg-6">
+	<p>
+		Per poder crear una pàgina web, és indispensable tenir un equip de disseny. En el nostre cas, dins del projecte Treballs als barris, col·laborem amb les especialitats de Disseny 2D/3D i amb Animació de productes audiovisuals 2D/3D. Ens comuniquem entre tots a través de l’aplicació de Google Hangouts, per tal d’aclarir conceptes de la web i les tasques que ha de dur a terme cada especialitat.
+	</p>
+</div>
+<div class="col-12 col-lg-6">
+	<p>
+		Després de moltes setmanes de treball, arriba el moment de penjar la web a internet. Per fer-ho, fem servir el programa Filezilla, que permet que ens connectem a un servidor web. A través d’aquest programa, pengem els arxius i el codi que formen la web al hosting, és a dir, a un servei d’emmagatzematge online. Aquest pas ens permet accedir a la web en qualsevol moment a través d’internet. 
+	</p>
+</div>	
+
 
 
 
