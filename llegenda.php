f0057515e36d7053ef6010da4476a9f075f4ee0c
claudiadel <cdelbarr@gmail.com>
2018-04-17 12:19:54 +0200

    Cambio de maquetación

--- llegenda.php
+++ llegenda.php
@@ -1,24 +1,21 @@
 <?php include("inc/head.php");?>
 	<div class="row row-eq-height row1">
-		<div class="col-12 col-md-6 col-lg-6 mt-4 order-2 order-md-0"><div class="txtTitles text-center d-none d-sm-block">La llegenda de Sant Jordi</div>
+		<div class="col-lg-8 offset-2 mt-3 mb-3 px-5 py-2"><div class="txtTitles text-center d-none d-sm-block">La llegenda de Sant Jordi</div>
+		<div class="row">
+			<div class="col-sm-6 col-md-6 col-lg-6 pr-5 order-2 order-md-0">
 			<div class="separation"></div>
 			<div class="text-center"><img src="img/llegenda_divider_lg.png" class="col-sm-12 col-md-6 col-lg-6 img-fluid"></div>
 			<div class="separation"></div>
-			<p class="txtBorders"><span class="firstcharacter">C</span>onta la llegenda que Sant Jordi era un soldat romà nascut al Pròxim Orient i és el gran protagonista d'una gran gesta cavalleresca que se situa a Líbia. Tot i això, certa tradició catalana la creu esdevinguda a la població de Montblanc.<br><br>
-
-			Diuen que assolava els voltants de la vila un drac ferotge i terrible, que posseïa les facultats de caminar, volar i nedar, i tenia l'alè pudent, fins al punt que des de molt lluny amb les seves alenades enverinava l'aire, i produïa la mort de tots els qui el respiraven. Era l'estrall dels ramats i de les persones i per tota aquella contrada regnava el terror més profund. Els habitants van pensar que si li donaven cada dia una persona que li serviria de presa, no faria l'estrall a tort i a dret. De fet, la llegenda diu, que el sistema els va sortir d'allò més bé, però el que era realment complicat, era de trobar una persona que cada dia es deixés menjar per aquell monstre. Tot el veïnat va decidir doncs, de fer cada dia un sorteig entre tots els habitants de la vila i que aquell que destinés la sort seria lliurat a la "simpàtica" fera.</p>
-			<p class="txtBorders">
-			Així es va fer durant molt de temps i el monstre se'n devia sentir satisfet, ja que deixava de fer els estralls i malvestats que havia fet abans. Però... vet aquí que un dia, la sort va voler que la filla del rei fos la destinada. La princesa era jove i bella. Hi hagué ciutadans que es van oferir per reemplaçar-la, però el rei fou sever i inexorable, i amb el cor ple de dol, va dir que tant era la seva filla com la de qualsevol dels seus súbdits i s'avingué que fos sacrificada. La donzella eixí de la ciutat i ella soleta s'encaminà cap al llac on residia la fera, mentre tot el veïnat, desconsolat i afligit, mirava des de la muralla com se n'anava al sacrifici.<br>
-
-			Però fou el cas que, quan va ésser un xic enllà de la muralla, se li presentà un jove cavaller, cavalcant un cavall blanc, i amb una armadura tota daurada i lluent. La donzella, tota preocupada, li digué que fugís ràpidament, ja que per allí rondava un monstre que així que el veiés se'l menjaria. El cavaller li digué que no temés, que no li havia de passar res, ni a ell ni a ella, ja que havia vingut expressament per combatre la fera i així alliberar del sacrifici de la princesa, com també a la vila de Montblanc. La fera, va sortir de cop i volta amb gran horror de la donzella i amb gran goig del cavaller.</p>
+			<p class="txtBorders"><span class="firstcharacter">L</span>eyendas de Sant Jordi hay muchas, y todas muy distintas, pero a nuestros oídos ha llegado una muy peculiar. Cuenta la leyenda que en las afueras de una pequeña aldea acechaba un terrible dragón que atemorizaba a todos los habitantes. Con sus grandes colmillos era capaz de devorar a todo el que se le pusiera por delante. Al pueblo se le ocurrió hacer un sorteo diario entre todos los habitantes y la persona que saliera sería la presa del dragón, para que así, a cambio de sacrificar a alguien, dejara a todos tranquilos. Y así se hizo durante mucho tiempo, hasta que un día la elegida en el sorteo fue la hija del rey. El rey, dolido, aprobó el sacrificio de su hija, y ésta se fue a las afueras para encontrarse con ese feroz dragón. De pronto, la princesa vio aparecer a un joven y apuesto caballero que estaba allí expresamente para matar al dragón y salvar a la princesa, y, tras una batalla entre el dragón y el caballero, finalmente el caballero hundió su espada en el cuerpo de la bestia. De la sangre que salió del animal, brotó un rosal, con las rosas más rojas jamás vistas. El caballero le regaló a la princesa una rosa y, tras llevarla al pueblo con el dragón a cargas como símbolo de victoria, el caballero desapareció misteriosamente y sin dejar rastro.<p class="txtBorders">
 		</div>
 		<div class="col-12 col-md-6 col-lg-6 mt-1 imgSize fill order-1"><img src="img/llegenda_gif_lg.gif" class="img-fluid">
 		</div>
-		<div class="col-12 mt-1 order-3">
+<!-- 		<div class="col-12 mt-1 order-3">
 			<p class="txtBorders">
 			Va començar una intensa però breu lluita, fins que el cavaller li va clavar una bona estocada amb la seva llança, que va deixar malferida a la terrible bèstia. El cavaller, que era Sant Jordi, lligà el monstre pel coll i la donà a la donzella perquè ella mateixa la portés a la ciutat, i la fera seguí tota mansa i atemorida. La llegenda explica fins i tot, que els habitants de la vila havien vist tota aquella gesta des de la muralla i que rebé amb els braços oberts a la donzella i el cavaller. A la plaça major de la vila, els vilatans van acabar de rematar aquell ferotge animal. De la sang que en brollà, en sorgí ràpidament un roser, amb les roses més vermelles que la princesa hagués vist mai. D'aquest roser el jove cavaller tallà una rosa i l'oferí a la princesa.
 
 			Es diu que el rei va voler casar la seva filla amb Sant Jordi, però que aquest li va replicar que no la mereixia i que la seva visita en aquella vila era perquè havia tingut una revelació divina sobre la necessitat urgent de salvar la vila del monstre. Recomanà al rei i als seus vassalls que fossin bons cristians i que honressin i veneressin Déu tal com mereixia. Desaparegué misteriosament tal com havia vingut.</p>
+		</div> -->
 		</div>
 	</div>
 
