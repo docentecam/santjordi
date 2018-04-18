e1eda9ff13609587fc685f474e5f8f113f1f3e47
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-04-18 15:08:07 +0200

    cosas hechas del apartado web

--- web.php
+++ web.php
@@ -4,8 +4,8 @@
 	<div class="col-12 d-none d-sm-block txtTitles text-center mb-5"><h1>Procés de creació de la pàgina</h1></div>
 </div>
 	<div class="col-lg-8 offset-2 mb-5 ">
-		<img class="img-fluid borderRound" src="img/fotodelgrupopaint2.png">
-		<h5 class="text-center ml-3 mr-3">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</h5>
+		<img class="img-fluid borderRound" src="img/fotoEnGrupoPaint.jpg">
+		<h5 class="text-center ml-3 mr-3"><span class="CA">Hello world! Som el Rafat, l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</h5></span>
 	</div>
 	<div class="col-8 offset-2">
 		<div class="row">
@@ -43,23 +43,41 @@
 	</div>
 
 <div class="row">
-	<div class="col-lg-4 offset-2 mt-5">
+	<div class="row">
+		
+		<div class="col-12 offset-10 d-none d-sm-block txtTitles text-center mb-5"><h1>Inici del projecte</h1></div>
+	</div>
+	<div class="row">	
+		<div class="col-lg-4 offset-2 mt-5 text-center">
 		<p>
-			Per començar el projecte, els components del grup ens vam reunir per fer una pluja d’idees o brainstorming. Dins d’aquesta primera part es plantegen les característiques de la pàgina web que volem crear, des de la seva finalitat (explicar la llegenda de Sant Jordi, la festivitat, etc) fins a la quantitat d’informació que volem afegir (imatges, textos, etc).
+			Per començar el projecte, tots els components del grup ens vam reunir per fer una pluja d’idees o brainstorming. Dins d’aquesta primera part es plantegen les característiques de la pàgina web que volem crear, des de la seva finalitat (explicar la llegenda de Sant Jordi, la festivitat, etc) fins a la quantitat d’informació que volem afegir (imatges, textos, etc). Més tart ens repartim els apartats establerts de la página.
 		</p>
 	</div>
-	<div class=" col-lg-4 mt-5">
+	<div class=" col-lg-3 mt-5">
 		<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
 	</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-4 offset-2 mt-5 text-center">
+			<p>
+				No obstant ens dividim cada apartat de la pàgina entre els integrants del grup amb el fi de desenvolupar individualment el lloc web. Tenint en compte les mides de cada imatge, gif, i vídeos animats implementats en aquesta pàgina que seran creats per les altres especialitats digitals. Tot això amb cohesió de grup.
+			</p>
+		</div>
+		<div class="col-lg-3 mt-4">
+			<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
+		</div>
+	</div>
+</div>
+<div class="row">
+	
 </div>
 </div>	
 <div id=desgWeb2 class="row mostraDiv">
 	<div class="col-12">
 			<a href="javascript:tancarDesglossament('desgWeb2');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
 	</div>
-	
 	<p>
-		A l’hora de fer un esquema de com volem que sigui la pàgina, fem servir el programa Mockingbot. Aquest programa ens dóna eines que ens faciliten crear l’estructura o wireframe, on apareixeran tots els elements visuals de la web. Els elements poden anar des d’un menú a la capçalera amb opcions de navegació, a un peu de pàgina amb els logotips corporatius, entre altres.
+		A l’hora de fer un esquema de com volem que sigui la pàgina, fem servir el programa Mockingbot un dels programas més utilizats pels maquetadors i disenyadors. Aquest programa ens dóna eines que ens faciliten crear l’estructura o wireframe, on apareixeran tots els elements visuals de la web. Els elements poden anar des d’un menú a la capçalera amb opcions de navegació, a un peu de pàgina amb els logotips corporatius, entre altres. En altres paraules l'aplicació Moockingbot ens permet visualitzar gràficament com quedaria la pàgina web (l'estructura) en aquest la web de Sant Jordi.
 	</p>
 </div>
 <div id="desgWeb3" class="row mostraDiv">
@@ -75,13 +93,15 @@
 			<a href="javascript:tancarDesglossament('desgWeb4');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
 		</div>
 	<div class="row">	
-		<div id="textoFinal" class="col-lg-4 offset-2">
+		<div id="textoFinal" class="col-lg-4 offset-2 text-center mt-5">
+			<div class="mt-5">
 			<p>
-				Després de moltes setmanes de treball, arriba el moment de penjar la web a internet. Per fer-ho, fem servir el programa Filezilla, que permet que ens connectem a un servidor web. A través d’aquest programa, pengem els arxius i el codi que formen la web al hosting, és a dir, a un servei d’emmagatzematge online. Aquest pas ens permet accedir a la web en qualsevol moment a través d’internet. 
+				Després de moltes setmanes de treball, arriba el moment de penjar la web a internet. Per fer-ho, fem servir el programa Filezilla, que permet que ens connectem a un servidor web. A través d’aquest programa, pengem els arxius i el codi que formen la web al hosting, és a dir, a un servei d’emmagatzematge online. Aquest pas ens permet accedir a la web en qualsevol moment a través d’internet.
 			</p>
+			</div>
 		</div>
-		<div class=" col-lg-4 ">
-			<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound">
+		<div class=" col-lg-4 mt-4">
+			<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound mb-5">
 		</div>
 		<div class="row">
 			<div class=" col-lg-4 offset-6">
