63ebe4d434906c94c0a9f56b439c049607cc5743
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-04-19 14:14:37 +0200

    1er apartado y 2do de web acabado

--- web.php
+++ web.php
@@ -1,43 +1,43 @@
 <?php include ("inc/head.php");?>
 
 <div class="row">
-<<<<<<< HEAD
-	<div class="col-12 d-none d-sm-block txtTitles text-center mb-5"><h1><span class="CA">Procés de creació de la pàgina</span></h1>
-	<h1><span class="ES">Proceso de creación de la página</span></h1>
+	<div class="col-12 d-none d-sm-block txtTitles text-center mb-5">
+		<h1><span class="CA" style="display:none;">Procés de creació de la pàgina</span>
+		<span class="ES" style="display:none;">Proceso de creación de la página</span></h1>
 	</div>
 </div>
 	<div class="col-lg-8 offset-lg-2 mb-5 ">
-		<img class="img-fluid borderRound" src="img/fotoEnGrupoPaint.jpg">
-		<h5 class="text-center ml-3 mr-3"><span class="CA">Hello world! Som el Rafat, <b>l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia</b>, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</h5></span><h5><span class="ES">Hello world! Somos  <b>Rafat, Andrea, Raúl, Raquel, Oscar, Santi y Clàudia</b>, los creadores de esta página web. Formamos parte de la especialidad de desarrollo de app y web apps, dentro del proyecto de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</span></h5>
+		<img class="img-fluid borderRound" src="img/fotogrupo2.jpg">
+		<h5 class="text-center ml-3 mr-3"><span class="CA" style="display:none;">Hello world! Som el Rafat, <b>l’Andrea, el Raúl, la Raquel, l’Oscar, el Santi i la Clàudia</b>, els creadors d’aquesta pàgina web. Formem part de l'especialitat de desenvolupament d'app i web apps, dins del projecte de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</span><span class="ES" style="display:none;">Hello world! Somos  <b>Rafat, Andrea, Raúl, Raquel, Oscar, Santi y Clàudia</b>, los creadores de esta página web. Formamos parte de la especialidad de desarrollo de app y web apps, dentro del proyecto de Treballs als barris de la Casa d’Oficis de Barcelona Activa.</span></h5>
 	</div>
 	<div class="col-lg-8 offset-lg-2">  
 		<div class="row">
 			<div class="col-12 col-lg-5 mb-4 mt-2">
 				<a href="javascript:mostrarDesglossament('desgWeb1');">
-					<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound"></a>
-					<h5 class="text-center"><span class="CA">1-Iniciar el projecte</span></h5><h5 class="text-center"><span class="ES">1-Iniciar el proyecto</span></h5>
+					<img src="img/fotoReunion.JPG" class="img-fluid borderRound"></a>
+					<h5 class="text-center"><span class="CA" style="display:none;">1-Iniciar el projecte</span><span class="ES" style="display:none;">1-Iniciar el proyecto</span></h5>
 			</div>
 			<div class="col-lg-5 offset-lg-2 mt-2">
 
 				<a href="javascript:mostrarDesglossament('desgWeb2');">
 					<img src="img/Capturamockingboot2.JPG" class="img-fluid borderRound"></a>
-					<h5 class="text-center"><span class="CA">2-Crear una estructura</h5></span><h5 class="text-center"><span class="ES">2-Crear una estructura</h5></span>	
+					<h5 class="text-center"><span class="CA" style="display:none;">2-Crear una estructura</span><span class="ES" style="display:none;">2-Crear una estructura</h5></span>	
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12 col-lg-5 mt-3">
 			<a href="javascript:mostrarDesglossament('desgWeb3');">
-				<img src="img/Capturahangoutconferencia2.JPG" class="img-fluid borderRound"></a>
-					<h5 class="text-center"><span class="CA">3-Posar en comú</h5></span><h5 class="text-center"><span class="ES">3-Poner en común</span></h5>
+				<img src="img/CapturaHangout.JPG" class="img-fluid borderRound"></a>
+					<h5 class="text-center"><span class="CA" style="display:none;">3-Posar en comú</span><span class="ES" style="display:none;">3-Poner en común</span></h5>
 			</div>
 
 			
 
 			<div class="col-lg-5 offset-lg-2 mt-3">
 
-			<a href="javascript:mostrarDesglossament('desgWeb4');">	
-				<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound"></a>
-					<h5 class="text-center"><span class="CA">4-Finalitzar el projecte</h5></span><h5 class="text-center"><span class="ES">4-Finalizar el proyecto</span></h5>
+				<a href="javascript:mostrarDesglossament('desgWeb4');">	
+					<img src="img/Capturafilezila4.JPG" class="img-fluid borderRound"></a>
+					<h5 class="text-center"><span class="CA" style="display:none;">4-Finalitzar el projecte</span><span class="ES" style="display:none;">4-Finalizar el proyecto</span></h5>
 			</div>
 		</div>
 	</div>
@@ -50,111 +50,110 @@
 
 <div class="row">
 	<div class="col-12 d-none d-sm-block txtTitles text-center ">
-			<h1>Inici del projecte</h1>
+		<h1><span class="CA" style="display:none;">Inici del projecte</span><span class="ES" style="display:none;">Inicio del proyecto</span></h1>
 	</div>
 	<div class="row">	
-		<div class="col-lg-4 offset-2 mt-5 text-center">
-		<p>
-			<span class="CA">
-				Per començar el projecte, tots els components del grup ens vam reunir per fer una pluja d'idees o brainstorming. Dins d'aquesta primera part es plantegen les característiques de la pàgina web que volem crear, des de la seva finalitat (explicar la llegenda de Sant Jordi, la festivitat, etc.) fins a la quantitat d'informació que volem afegir (imatges, textos, etc). Més tard ens repartim els apartats establerts de la pàgina.
-			</span>
-		</p>
-		<p>
-			<span class="ES">
-				Para empezar el proyecto, todos los componentes del grupo nos reunimos para hacer una lluvia de ideas o brainstorming. Dentro de esta primera parte se plantean las características de la página web que queremos crear, desde su finalidad (explicar la leyenda de Sant Jordi, la festividad, etc) hasta la cantidad de información que queremos añadir (imágenes, textos, etc). Más tarde nos repartimos los apartados establecidos de la página.
-			</span>
-		</p>
-	</div>
-	<div class=" col-lg-3 mt-5">
-		<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
-	</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-4 offset-2 mt-5 text-center">
+		<div class="col-lg-6 offset-3 mt-5 text-center">
 			<p>
-				<span class="CA">
-					No obstant ens dividim cada apartat de la pàgina entre els integrants del grup amb el fi de desenvolupar individualment el lloc web. Tenint en compte les mides de cada imatge, gif, i vídeos animats implementats en aquesta pàgina que seran creats per les altres especialitats digitals. Tot això sobretot amb cohesió de grup.
+				<span class="CA" style="display:none;">
+					Per començar el projecte, tots els components del grup ens vam reunir per fer una pluja d'idees o brainstorming. Dins d'aquesta primera part es plantegen les característiques de la pàgina web que volem crear, des de la seva finalitat (explicar la llegenda de Sant Jordi, la festivitat, etc.) fins a la quantitat d'informació que volem afegir (imatges, textos, etc). Més tard ens repartim els apartats establerts de la pàgina.
+				</span>
+			
+			
+				<span class="ES" style="display:none;">
+					Para empezar el proyecto, todos los componentes del grupo nos reunimos para hacer una lluvia de ideas o brainstorming. Dentro de esta primera parte se plantean las características de la página web que queremos crear, desde su finalidad (explicar la leyenda de Sant Jordi, la festividad, etc) hasta la cantidad de información que queremos añadir (imágenes, textos, etc). Más tarde nos repartimos los apartados establecidos de la página.
 				</span>
 			</p>
 			<p>
-				<span class="ES">
+				<span class="CA" style="display:none;">
+					No obstant ens dividim cada apartat de la pàgina entre els integrants del grup amb el fi de desenvolupar individualment el lloc web. Tenint en compte les mides de cada imatge, gif, i vídeos animats implementats en aquesta pàgina que seran creats per les altres especialitats digitals. Tot això sobretot amb cohesió de grup.
+				</span>
+			
+				<span class="ES" style="display:none;">
 					No obstante nos dividimos cada apartado de la página entre los integrantes del grupo con el fin de desarrollar individualmente el sitio web. Teniendo en cuenta las medidas de cada imagen, gif, y vídeos animados implementados en esta página que serán creados por las otras especialidades digitales. Todo esto sobretodo con cohesión de grupo.
 				</span>
 			</p>
 		</div>
-		<div class="col-lg-3 mt-4">
-			<img src="img/definicióProjecteoriginal.jpg" class="img-fluid borderRound">
+	</div>
+	<div class="row">
+		<div class=" col-lg-3 offset-3 mt-5">
+			<img src="img/fotoTrabajandoSinRecortar.JPG" class="img-fluid borderRound">
+		</div>
+		<div class="col-lg-3 mt-5">
+			<img src="img/fotoTrabajando.JPG" class="img-fluid borderRound">
 		</div>
 	</div>
+	</div>
 
-</div>
 </div>	
 <div id=desgWeb2 class="row mostraDiv">
 	<div class="col-12">
 			<a href="javascript:tancarDesglossament('desgWeb2');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
 	</div>
 	<div class="col-12 d-none d-sm-block txtTitles text-center ">
-			<h1>Creació de la estructura</h1>
+			<h1><span class="CA" style="display:none;">Creació de la estructura</span></h1>
+			<h1><span class="ES" style="display:none;">Creación de la estructura</span></h1>
 	</div>
-<div class="row">
-	<div class="col-lg-7 offset-3 mt-5 text-center">
-		<p>
-			<span class="CA">
-			A l'hora de fer un esquema de com volem que sigui la pàgina, fem servir el programa Mockingbot un dels programes més utilitzats pels programadors i dissenyadors. Aquest programa ens dóna eines que ens faciliten crear l'estructura o wireframe, on apareixeran tots els elements visuals de la web. Els elements poden anar des d'un menú a la capçalera amb opcions de navegació, a un peu de pàgina amb els logotips corporatius, entre altres. En altres paraules l'aplicació Mockingbot ens permet visualitzar gràficament com quedaria la pàgina web (l'estructura) en aquest cas la web de Sant Jordi.
-			</span>
-		</p>
-		<p>
-			<span class="ES">
-				A la hora de hacer un esquema de cómo queremos que sea la página, usamos el programa Mockingbot uno de los programas más utilizados por los programadores y diseñadores. Este programa nos da herramientas que nos facilitan crear la estructura o wireframe, donde aparecerán todos los elementos visuales de la web. Los elementos pueden ir desde un menú a la cabecera con opciones de navegación, a un pie de página con los logotipos corporativos, entre otros. En otras palabras la aplicación Mockingbot nos permite visualizar gráficamente como quedaría la página web (la estructura) en este caso la web de Sant Jordi.
-			</span>
-		</p>
+	<div class="row">
+		<div class="col-lg-7 offset-3 mt-5 text-center">
+			<p>
+				<span class="CA" style="display:none;">
+					A l'hora de fer un esquema de com volem que sigui la pàgina, fem servir el programa Mockingbot un dels programes més utilitzats pels programadors i dissenyadors. Aquest programa ens dóna eines que ens faciliten crear l'estructura o wireframe, on apareixeran tots els elements visuals de la web. Els elements poden anar des d'un menú a la capçalera amb opcions de navegació, a un peu de pàgina amb els logotips corporatius, entre altres. En altres paraules l'aplicació Mockingbot ens permet visualitzar gràficament com quedaria la pàgina web (l'estructura) en aquest cas la web de Sant Jordi.
+				</span>
+				<span class="ES" style="display:none;">
+					A la hora de hacer un esquema de cómo queremos que sea la página, usamos el programa Mockingbot uno de los programas más utilizados por los programadores y diseñadores. Este programa nos da herramientas que nos facilitan crear la estructura o wireframe, donde aparecerán todos los elementos visuales de la web. Los elementos pueden ir desde un menú a la cabecera con opciones de navegación, a un pie de página con los logotipos corporativos, entre otros. En otras palabras la aplicación Mockingbot nos permite visualizar gráficamente como quedaría la página web (la estructura) en este caso la web de Sant Jordi.
+				</span>
+			</p>
+		</div>
+		<div class="col-lg-4 offset-3 mt-5 text-center">
+			<img src="img/Capturamockingboot2.JPG" class="img-fluid borderRound">
+		</div>	
+		<div class="col-lg-3  mt-5 text-center">
+			<img src="img/Capturamockingbot4.JPG" class="img-fluid borderRound">
+		</div>	
 	</div>
-	<div class="col-lg-4 offset-3 mt-3 text-center">
-		<img src="img/Capturamockingboot2.JPG" class="img-fluid borderRound">
-	</div>	
-	<div class="col-lg-3  mt-3 text-center">
-		<img src="img/Capturamockingbot4.JPG" class="img-fluid borderRound">
-	</div>	
-</div>
 </div>
 <div id="desgWeb3" class="row mostraDiv">
 	<div class="col-12 ">
-			<a href="javascript:tancarDesglossament('desgWeb3');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
+		<a href="javascript:tancarDesglossament('desgWeb3');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
 	</div>
 	<div class="col-12 d-none d-sm-block txtTitles text-center ">
-			<h1>Posada en comú</h1>
-		</div>
-	<p>
-		<span class="CA">
-			Per poder crear una pàgina web, és indispensable tenir un equip de disseny. En el nostre cas, dins del projecte Treballs als barris, col·laborem amb les especialitats de Disseny 2D/3D i amb Animació de productes audiovisuals 2D/3D. Ens comuniquem entre tots a través de l’aplicació de Google Hangouts, per tal d’aclarir conceptes de la web i les tasques que ha de dur a terme cada especialitat.
-		</span>	
-	</p>
-	<p>
-		<span class="ES">
-			Para poder crear una página web, es indispensable tener un equipo de diseño. En nuestro caso, dentro del proyecte Trabajos en los barrios, colaboramos con las especialidades de Diseño 2D/3D y con Animación de productos audiovisuales 2D/3D. Nos comunicamos entre todos a través de la aplicación de Google Hangouts, para aclarar conceptos de la web y las tareas que tiene que llevar a cabo cada especialidad.
-		</span>
-	</p>
+		<h1><span class="CA" style="display:none;">Posada en comú</span>
+		<span class="ES" style="display:none;">Puesta en común</span></h1>
+	</div>
+	<div class="col-lg-6 text-center offset-3">
+		<p>
+			<span class="CA" style="display:none;">
+				Per poder crear una pàgina web, és indispensable tenir un equip de disseny. En el nostre cas, dins del projecte Treballs als barris, col·laborem amb les especialitats de Disseny 2D/3D i amb Animació de productes audiovisuals 2D/3D. En conseqüència necessitem l'ajuda de tot l'equip digital i una eina idònia per comunicar-nos entre tots a través de l'aplicació gratuïta de Google hangouts(una aplicació web per fer videoconferències i xatejar), necessària per aclarir dubtes, conceptes i aspectes sobre la web i engegar les tasques que ha de dur a terme cada especialitat.
+			</span>	
+			<span class="ES" style="display:none;">
+				Para poder crear una página web, es indispensable tener un equipo de diseño. En nuestro caso, dentro del proyecte Trabajos en los barrios, colaboramos con las especialidades de Diseño 2D/3D y con Animación de productos audiovisuales 2D/3D. Nos comunicamos entre todos a través de la aplicación de Google Hangouts, para aclarar conceptos de la web y las tareas que tiene que llevar a cabo cada especialidad.
+			</span>
+		</p>
+	</div>
+	<div class="col-12 col-lg-6 offset-3">
+		<img src="img/CapturaHangout.JPG" class="img-fluid borderRound">
+	</div>
 </div>
 <div id="desgWeb4" class="row mostraDiv">
-		<div class="col-lg-12 ">
-			<a href="javascript:tancarDesglossament('desgWeb4');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
-		</div>
+	<div class="col-lg-12 ">
+		<a href="javascript:tancarDesglossament('desgWeb4');"><img src="img/cartells/proposta2TancarReduida.png" class="img-fluid"></a>
+	</div>
 		<div class="col-12 d-none d-sm-block txtTitles text-center ">
-			<h1>Finalització del projecte</h1>
+			<h1><span class="CA" style="display:none;">Finalització del projecte</span>
+			<span class="ES" style="display:none;">Finalización del proyecto</span></h1>
 		</div>
 	<div class="row">	
 		<div id="textoFinal" class="col-lg-4 offset-2 text-center mt-5">
 			<div class="mt-5">
-			<p>
-				<span class="CA">
-					Després de moltes setmanes de treball, arriba el moment de penjar la web a internet. Per fer-ho, fem servir el programa Filezilla, que permet que ens connectem a un servidor web. A través d’aquest programa, pengem els arxius i el codi que formen la web al hosting, és a dir, a un servei d’emmagatzematge online. Aquest pas ens permet accedir a la web en qualsevol moment a través d’internet.
-				</span>
-			</p>
-			<p>
-				<span class="ES">
-					Después de muchas semanas de trabajo, llega el momento de colgar la web a internet. Para hacerlo, usamos el programa Filezilla, que permite que nos conectamos a un servidor web. A través de este programa, colgamos los archivos y el código que forman la web al hosting, es decir, a un servicio de almacenamiento online. Este paso nos permite acceder en la web en cualquier momento a través de internet.
-				</span>
-			</p>
+				<p>
+					<span class="CA" style="display:none;">
+						Després de moltes setmanes de treball, arriba el moment de penjar la web a internet. Per fer-ho, fem servir el programa Filezilla, que permet que ens connectem a un servidor web. A través d’aquest programa, pengem els arxius i el codi que formen la web al hosting, és a dir, a un servei d’emmagatzematge online. Aquest pas ens permet accedir a la web en qualsevol moment a través d’internet.
+					</span>
+					<span class="ES" style="display:none;">
+						Después de muchas semanas de trabajo, llega el momento de colgar la web a internet. Para hacerlo, usamos el programa Filezilla, que permite que nos conectamos a un servidor web. A través de este programa, colgamos los archivos y el código que forman la web al hosting, es decir, a un servicio de almacenamiento online. Este paso nos permite acceder en la web en cualquier momento a través de internet.
+					</span>
+				</p>
 			</div>
 		</div>
 		<div class=" col-lg-4 mt-4">
