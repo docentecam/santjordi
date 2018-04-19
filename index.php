51999e273cee07897efae6b7e15594421e899e66
rtalyon <rta.lyon@gmail.com>
2018-04-19 07:55:18 +0200

    quitado titulos carousel

--- index.php
+++ index.php
@@ -18,22 +18,10 @@
 			  </div> 
 		    </div> -->
 		    <div id="carouselImg2" class="carousel-item  active">
-		      <a href="llegenda.php"><img class="img-fluid" src="img/carousel_lleg.jpg" alt="Accès a la llegenda"></a>
-		      <div class="carousel-caption d-none d-md-block">
-			    <h5>
-			    		<span class="CA">Llegenda de Sant Jordi</span>
-			    		<span class="ES">Leyenda de Sant Jordi</span>
-			    </h5>
-			  </div> 
+		      	<a href="llegenda.php"><img class="img-fluid" src="img/carousel_lleg.jpg" alt="Accès a la llegenda"></a>
 		    </div>
 		    <div id="carouselImg3" class="carousel-item">
-		     <a href="festivitat.php"><img class="img-fluid" src="img/carousel_fest.jpg" alt="Accès a la festivitat"></a>
-		      <div class="carousel-caption d-none d-md-block">
-			      <h5>
-			    		<span class="CA">Festivitat de Sant Jordi</span>
-			    		<span class="ES">Festividad de Sant Jordi</span>
-			    </h5>
-			  </div>  
+		     	<a href="festivitat.php"><img class="img-fluid" src="img/carousel_fest.jpg" alt="Accès a la festivitat"></a> 
 		    </div>
 		 </div>
 		 <a class="carousel-control-prev" href="#carouselSantJordi" role="button" data-slide="prev">
