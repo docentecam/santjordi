9533071b599750859664e08a5bfcd8414d52ce96
rafatbd <rafatinfo4@gmail.com>
2018-04-19 13:55:27 +0200

    cartells modal versio 1

--- inc/head.php
+++ inc/head.php
@@ -8,43 +8,6 @@
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="icon" href="img/favicon.png" type="image/x-icon">
-		<script type="text/javascript">
-			$('#myModal').on('shown.bs.modal', function () {
-  				$('#myInput').trigger('focus')
-			})
-			function mostrarDesglossament(idDiv) {
-	            divMostrar = document.getElementById(idDiv);
-	            divMostrar.style.display = 'block';
-	            console.log("arriba");
-	        }
-
-	        function tancarDesglossament(idDiv) {
-	            divOcultar = document.getElementById(idDiv);
-	            divOcultar.style.display = 'none';
-	        }
-	        function canviaIdioma(idioma) {
-					
-					var ocultaCA;
-					var ocultaES;
-					
-					if(idioma) {
-						ocultaCA="none";
-						ocultaES="inline";
-					}
-					else{
-						ocultaCA="inline";
-						ocultaES="none";
-					}
-						objetosCA=document.getElementsByClassName("CA");
-						objetosES=document.getElementsByClassName("ES");
-
-					for(i=0;i<objetosCA.length;i++){
-						objetosCA[i].style.display=ocultaCA;
-						objetosES[i].style.display=ocultaES;
-					}
-			}
-					       
-		</script>
 	</head>
 	<body>
 		<?php include("header.php");?>
--- js/funcions.js
+++ js/funcions.js
@@ -37,4 +37,46 @@
 						objetosES[i].style.display=ocultaES;
 					}
 			}
+
+			// window_layer{
+			function muestraDes()
+			{
+				var modal = document.getElementById('myModal');
+				img.onclick = function(){
+				    modal.style.display = "block";
+				    modalImg.src = this.src;
+				    //captionText.innerHTML = this.alt;
+				}
+				var span = document.getElementsByClassName("close")[0];
+				span.onclick = function() {
+				  modal.style.display = "none";
+				}
+			}
+
+
+			// Get the modal
+			var modal = document.getElementById('myModal');
+
+			// Get the image and insert it inside the modal - use its "alt" text as a caption
+			var img = document.getElementById('myImg');
+			var modalImg = document.getElementById("img01");
+			//var captionText = document.getElementById("caption");
+			img.onclick = function(){
+			    modal.style.display = "block";
+			    //modalImg.src = this.src;
+			    //captionText.innerHTML = this.alt;
+			}
+
+			// Get the <span> element that closes the modal
+			var span = document.getElementsByClassName("close")[0];
+
+			// When the user clicks on <span> (x), close the modal
+			span.onclick = function() {
+			  modal.style.display = "none";
+			}
+			//}
+
+
+
+
 					       
