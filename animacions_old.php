cfd124e5f0e43685272ca7cde5be55ba7076be9a
yaibondi <yaibondi@gmail.com>
2018-04-20 11:51:00 +0200

    animacio javascript transparencia

--- animacions_old.php
+++ animacions_old.php
@@ -42,8 +42,8 @@
 
 	    	<div class="col-sm-12 col-md-4 col-lg-3">
 				<img class=" col-12 img-fluid" src="img/animacions/animacio2.png" alt="" title="" onclick="agrandaDiv(2);">
-			    <div  class="col-12 text-center"><span class="CA" style="display:none;"><b>Un Desconegut Inesperat</b></span><span class="ES" style="display:none;"><b>Un Desconocido Inesperado</b></span></div>
-				<div class="col-12 text-center mb-4">Iván Ruiz </div>
+			    <div  class="col-12 text-center"><span class="CA" style="display:none;"><b>Odissea a Barcelona</b></span><span class="ES" style="display:none;"><b>Odisea en Barcelona</b></span></div>
+				<div class="col-12 text-center mb-4">Adrià Morilla </div>
 				<div id="myModal2" class="modal">
 				  	<div class="close col-12 d-flex justify-content-end"><img src="img/btnCloseDiv.png" class="img-fluid" onclick="tancaDiv(2);"></div>
 				 	<div class="row">	
@@ -58,17 +58,16 @@
 					<div class="row">
 						<div class="col-4 col-md-2 offset-md-3"><img class="img-fluid mt-3" src="img/animacions/retrat2.png" alt="" title=""></div>
 						<div class="col-8 col-md-3 mt-4 mt-md-2 mt-lg-4 mt-xl-4 textWhite">
-							<h5><b>Iván Ruiz</b> </h5>
+							<h5><b>Adrià Morilla </b> </h5>
 							<p class="mt-sm-2 mt-md-3 mt-lg-4 ">
 								<span class="CA " style="display:none; ">
-								  	M&#39;he basat en els personatges d&#39;Hora d&#39;Aventures del Cartoon Network i en sèries animades
-									de la meva infància, afegint idees pròpies com per exemple la mort i el fantasma del drac.el
-									resultat final és un rework de la idea que tènia en principi.
+								  	He utilitzat un estil basat en el píxel art característic dels videojocs de les primeres
+									màquines recreatives. A més he introduït referències de pel·lícules de fantasia i ciència
+									ficció de les quals sóc fan.
 								</span><span class="ES" style="display:none;">
-									Me he basado en los personajes temprano de Aventuras del Cartoon Network y en
-									series animadas de mi infancia, añadiendo ideas propias como por ejemplo la
-									muerte y el fantasma del drac.el resultado final es un rework de la idea que tenia
-									en principio.
+									He utilizado un estilo basado en el pixel art característico de los videojuegos de las
+									primeras máquinas recreativas. Además he introducido referencias de películas de
+									fantasía y ciencia ficción de las cuales soy fan.
 								</span>
 							</p>
 						</div>  
@@ -76,116 +75,6 @@
 				</div>
 	    	</div>
 
-			 <div class="col-sm-12 col-md-4 col-lg-3">
-							<img class=" col-12 img-fluid" src="img/animacions/animacio3.png" alt="" title="" onclick="agrandaDiv(3);">
-						    <div  class="col-12 text-center"><span class="CA" style="display:none;"><b>L’aventura del Petit Cavaller</b></span><span class="ES" style="display:none;"><b>La aventura del Pequeño Caballero</b></span></div>
-							<div class="col-12 text-center mb-4">Eva Sopale</div>
-							<div id="myModal3" class="modal">
-							  	<div class="close col-12 d-flex justify-content-end"><img src="img/btnCloseDiv.png" class="img-fluid" onclick="tancaDiv(3);"></div>
-							 	<div class="row">	
-								  	<div class="col-12 col-md-6 offset-md-3">
-								  		<div class="row">
-											<div class="col-10 embed-responsive embed-responsive-16by9 mt-4">
-									  			<iframe class="embed-responsive-item" src="img/animacions/animacioAlumne3.mp4" allowfullscreen></iframe>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-4 col-md-2 offset-md-3"><img class="img-fluid mt-3" src="img/animacions/retrat3.png" alt="" title=""></div>
-									<div class="col-8 col-md-3 mt-4 mt-md-2 mt-lg-4 mt-xl-4 textWhite">
-										<h5><b>Eva Sopale </b> </h5>
-										<p class="mt-sm-2 mt-md-3 mt-lg-4 ">
-											<span class="CA " style="display:none; ">
-											M’he inspirat en les animacions clàssiques de finals dels anys vint, com, per exemple,
-											Mickey Mouse, Felix el gat o Betty Boop, ja que aquest estil em provoca curiositat i crec que
-											mai passa de moda.
-											Me he inspirado en las animaciones clásicas de finales de los años veinte, como,
-											por ejemplo, Mickey Mouse, Felix el gato o Betty Boop, puesto que este estilo me
-											provoca curiosidad y creo que nunca pasa de moda.
-											</span><span class="ES" style="display:none;">
-											Me he inspirado en las animaciones clásicas de finales de los años veinte, como,
-											por ejemplo, Mickey Mouse, Felix el gato o Betty Boop, puesto que este estilo me
-											provoca curiosidad y creo que nunca pasa de moda.
-											</span>
-										</p>
-									</div>  
-								</div>	
-							</div>
-				    	</div>
-						<div class="col-sm-12 col-md-4 col-lg-3">
-										<img class=" col-12 img-fluid" src="img/animacions/animacio4.png" alt="" title="" onclick="agrandaDiv(4);">
-									    <div  class="col-12 text-center"><span class="CA" style="display:none;"><b>Malson abans de Sant Jordi</b></span><span class="ES" style="display:none;"><b>Pesadilla antes de Sant Jordi</b></span></div>
-										<div class="col-12 text-center mb-4">Nicolás Torres </div>
-										<div id="myModal4" class="modal">
-										  	<div class="close col-12 d-flex justify-content-end"><img src="img/btnCloseDiv.png" class="img-fluid" onclick="tancaDiv(4);"></div>
-										 	<div class="row">	
-											  	<div class="col-12 col-md-6 offset-md-3">
-											  		<div class="row">
-														<div class="col-10 embed-responsive embed-responsive-16by9 mt-4">
-												  			<iframe class="embed-responsive-item" src="img/animacions/animacioAlumne4.mp4" allowfullscreen></iframe>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="row">
-												<div class="col-4 col-md-2 offset-md-3"><img class="img-fluid mt-3" src="img/animacions/retrat4.png" alt="" title=""></div>
-												<div class="col-8 col-md-3 mt-4 mt-md-2 mt-lg-4 mt-xl-4 textWhite">
-													<h5><b>Nicolás Torres </b> </h5>
-													<p class="mt-sm-2 mt-md-3 mt-lg-4 ">
-														<span class="CA " style="display:none; ">
-														  	He utilitzat un estil basat en el píxel art característic dels videojocs de les primeres
-															màquines recreatives. A més he introduït referències de pel·lícules de fantasia i ciència
-															ficció de les quals sóc fan.
-														</span><span class="ES" style="display:none;">
-															He utilizado un estilo basado en el pixel art característico de los videojuegos de las
-															primeras máquinas recreativas. Además he introducido referencias de películas de
-															fantasía y ciencia ficción de las cuales soy fan.
-														</span>
-													</p>
-												</div>  
-											</div>	
-										</div>
-							    	</div>
-									<div class="col-sm-12 col-md-4 col-lg-3">
-													<img class=" col-12 img-fluid" src="img/animacions/animacio5.png" alt="" title="" onclick="agrandaDiv(5);">
-												    <div  class="col-12 text-center"><span class="CA" style="display:none;"><b>Una Història Distinta</b></span><span class="ES" style="display:none;"><b>Una Historia Distinta</b></span></div>
-													<div class="col-12 text-center mb-4">Juan Reyes</div>
-													<div id="myModal5" class="modal">
-													  	<div class="close col-12 d-flex justify-content-end"><img src="img/btnCloseDiv.png" class="img-fluid" onclick="tancaDiv(5);"></div>
-													 	<div class="row">	
-														  	<div class="col-12 col-md-6 offset-md-3">
-														  		<div class="row">
-																	<div class="col-10 embed-responsive embed-responsive-16by9 mt-4">
-															  			<iframe class="embed-responsive-item" src="img/animacions/animacioAlumne5.mp4" allowfullscreen></iframe>
-																	</div>
-																</div>
-															</div>
-														</div>
-														<div class="row">
-															<div class="col-4 col-md-2 offset-md-3"><img class="img-fluid mt-3" src="img/animacions/retrat5.png" alt="" title=""></div>
-															<div class="col-8 col-md-3 mt-4 mt-md-2 mt-lg-4 mt-xl-4 textWhite">
-																<h5><b>Juan Reyes</b> </h5>
-																<p class="mt-sm-2 mt-md-3 mt-lg-4 ">
-																	<span class="CA " style="display:none; ">
-																	  	Per a aquesta animació de Sant Jordi vaig combinar part de la història principal de Sant
-																		Jordi amb una part alternativa on el drac té un paper molt diferent a allò que la gent de la
-																		vila pensava d&#39;ell.
-
-																		L&#39;animació té un estil de llibre desplegable on els escenaris es van formant a un ritme
-																		energètic acompanyats per la música.
-																	</span><span class="ES" style="display:none;">
-																		Para esta animación de Sant Jordi combiné parte de la historia principal de Sant
-																		Jordi con una parte alternativa donde el dragón tiene un papel muy diferente a
-																		aquello que la gente de la villa pensaba de él.
-																		La animación tiene un estilo de libro desplegable donde los escenarios se van
-																		formando a un ritmo energético acompañados por la música.
-																	</span>
-																</p>
-															</div>  
-														</div>	
-													</div>
-										    	</div>
 
 
 
@@ -204,36 +93,38 @@
 
 
 
-
-	<!-- 	    <div class="col-sm-12 col-md-4 col-lg-3">
-
+		    <div class="col-sm-12 col-md-4 col-lg-3">
+				<a class="noLink" href="javascript:mostrarDesglossament('desgAlumAni2');">
 			    	<img class=" col-12 img-fluid" src="img/animacions/animacio2.png" alt="" title="">
-				  	<div class="col-12 text-center"><span class="CA" style="display:none;"><b></b></span><span class="ES" style="display:none;"><b></b></span></div>
+			    </a>
+				  	<div class="col-12 text-center"><span class="CA" style="display:none;"><b>Un Desconegut Inesperat</b></span><span class="ES" style="display:none;"><b>Un Desconocido Inesperado</b></span></div>
 				    <div class="col-12 text-center mb-4">Iván Ruiz </div>
 				
-		    </div> -->
-		    <!-- <div class="col-sm-12 col-md-4 col-lg-3">
+		    </div>
+		    <div class="col-sm-12 col-md-4 col-lg-3">
+				<a class="noLink" href="javascript:mostrarDesglossament('desgAlumAni3');">
 			    	<img class=" col-12 img-fluid" src="img/animacions/animacio3.png" alt="" title="">
+			    </a>
 				  	<div class="col-12 text-center "><span class="CA" style="display:none;"><b>L’aventura del Petit Cavaller</b></span><span class="ES" style="display:none;"><b>La aventura del Pequeño Caballero</b></span></div>
 				    <div class="col-12 text-center mb-4">Eva Sopale  </div>
 				
-		    </div> -->
-		    <!-- <div class="col-sm-12 col-md-4 col-lg-3">
+		    </div>
+		    <div class="col-sm-12 col-md-4 col-lg-3">
 				<a class="noLink" href="javascript:mostrarDesglossament('desgAlumAni4');">
 			    	<img class=" col-12 img-fluid" src="img/animacions/animacio4.png" alt="" title="">
 			    </a>
 				  	<div class="col-12 text-center"><span class="CA" style="display:none;"><b>Malson abans de Sant Jordi</b></span><span class="ES" style="display:none;"><b>Pesadilla antes de Sant Jordi</b></span></div>
 				    <div class="col-12 text-center mb-4">Nicolás Torres </div>
 				
-		    </div> -->
-	<!-- 	    <div class="col-sm-12 col-md-4 col-lg-3">
+		    </div>
+		    <div class="col-sm-12 col-md-4 col-lg-3">
 				<a class="noLink" href="javascript:mostrarDesglossament('desgAlumAni5');">
 			    	<img class=" col-12 img-fluid" src="img/animacions/animacio5.png" alt="" title="">
 			    </a>
 				  	<div class="col-12 text-center"><span class="CA" style="display:none;"><b>Una Història Distinta</b></span><span class="ES" style="display:none;"><b>Una Historia Distinta</b></span></div>
 				    <div class="col-12 text-center mb-4">Juan Reyes </div>
 				
-		    </div> -->
+		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
 				
 			    	<img class=" col-12 img-fluid" src="img/animacions/animacio6.png" alt="" title="">
@@ -243,7 +134,7 @@
 		    </div>
 		    <div class="col-sm-12 col-md-4 col-lg-3">
 				
-			    	<img class="col-12 img-fluid" src="img/animacions/animacio7.png" alt="" title="">
+			    	<img class=" col-12 img-fluid" src="img/animacions/animacio7.png" alt="" title="">
 				  	<div class="col-12 text-center"><span class="CA" style="display:none;"><b>La llegenda del misteriós llibre per Sant Jordi</b></span><span class="ES" style="display:none;"><b>La leyenda del misterioso libro para Sant Jordi</b></span></div>
 				    <div class="col-12 text-center mb-4">Jordi Pecellin </div>
 				
@@ -258,7 +149,7 @@
 		    </div> -->
 
 		</div>
-<!-- 					
+					
 			<div id="desgAlumAni1" class="mostraDiv">
 					
 			</div>	
@@ -295,9 +186,9 @@
 								en principio.</p></span>
 						</div>
 				</div>	 
-			</div> -->
+			</div>
 
-			<!-- <div id="desgAlumAni3" class="mostraDiv">
+			<div id="desgAlumAni3" class="mostraDiv">
 				<div class="row">
 				  	<div class="col-sm-12 col-md-6 offset-md-3">
 					  	<div class="row">
@@ -404,7 +295,7 @@
 							    </p>
 						</div>
 				</div>
-			</div> -->
+			</div>
 			<!-- <div id="desgAlumAni6" class="mostraDiv">
 				<div class="row">
 				  	<div class="col-sm-12 col-md-6 offset-md-3">
